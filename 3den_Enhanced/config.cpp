17b713a474be9b00e8fc92508ea5d15e5bbd783f
robertseibel@outlook.de <robertseibel@outlook.de>
2019-05-18 12:55:37 +0200

    Debug console uses Enh_FunctionsViewer now

--- 3den_Enhanced/config.cpp
+++ 3den_Enhanced/config.cpp
@@ -17,3 +17,26 @@
 #include "GUI\Enh_placementTools.hpp"
 //Overloaded and enhanced vanilla GUIs
 #include "GUI\display3DENPublishMIssionSelectImage.hpp"
+
+//Delete the original Functions Viewer button in the BIS Debug Console and replace it
+class RscControlsGroup;
+class RscControlsGroupNoScrollbars;
+class RscShortcutButton;
+class RscButtonMenu;
+class RscDebugConsole: RscControlsGroupNoScrollbars 
+{
+	class Controls 
+    {
+		delete ButtonFunctions;
+		class Enh_FunctionsViewer: RscButtonMenu 
+        {
+			idc = -1;
+			onLoad = "(_this # 0) ctrlSetText localize 'STR_A3_RscDebugConsole_ButtonFunctions'";
+			onButtonClick = "ctrlParent (_this # 0) createDisplay 'Enh_FunctionsViewer'";
+			x = "7.5 * (((safezoneW / safezoneH) min 1.2) / 40)";
+			y = "19.4 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+			w = "7.4 * (((safezoneW / safezoneH) min 1.2) / 40)";
+			h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+		};
+	};
+};
