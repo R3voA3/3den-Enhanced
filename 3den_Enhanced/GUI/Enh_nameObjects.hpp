d9ba769bcc3bf708744aea2523bee5ecae3f2921
robertseibel@outlook.de <robertseibel@outlook.de>
2019-06-13 01:12:43 +0200

    Name Objects GUI uses pixelGrid now

--- 3den_Enhanced/GUI/Enh_nameObjects.hpp
+++ 3den_Enhanced/GUI/Enh_nameObjects.hpp
@@ -1,4 +1,94 @@
+#define DIALOG_W 70
+#define DIALOG_H 37
+
 class Enh_NameObjects
+{
+	idd = ENH_IDD_NAMEOBJECTS;
+	onLoad = "call Enh_fnc_nameObjects_onLoad";
+	movingEnable = true;
+	class ControlsBackground
+	{
+		DISABLE_BACKGROUND
+		class Background: ctrlStaticBackground
+		{
+			x = CENTERED_X(DIALOG_W);
+			y = DIALOG_TOP + 5 * GRID_H;
+			w = DIALOG_W * GRID_W;
+			h = DIALOG_H * GRID_H;
+		};
+	};
+	class Controls
+	{
+		class Header: ctrlStaticTitle
+		{
+			text = $STR_ENH_nameObjects_header;
+			x = CENTERED_X(DIALOG_W);
+			y = DIALOG_TOP;
+			w = DIALOG_W * GRID_W;
+			h = 5 * GRID_H;
+		};
+		class VariableName: ctrlStatic
+		{
+			text = $STR_ENH_briefingEditor_variableName_displayName;
+			x = CENTERED_X(DIALOG_W) + 1 * GRID_W;
+			y = DIALOG_TOP + 7 * GRID_H;
+			w = DIALOG_W * GRID_W - 4 * GRID_W;
+			h = 5 * GRID_H;
+		};
+		class VariableNameValue: ctrlEdit
+		{
+			idc = 1000;
+			x = CENTERED_X(DIALOG_W) + 2 * GRID_W;
+			y = DIALOG_TOP + 14 * GRID_H;
+			w = DIALOG_W * GRID_W - 4 * GRID_W;
+			h = 5 * GRID_H;
+		};
+		class IndexStart: ctrlStatic
+		{
+			text = $STR_ENH_nameObjects_startIndex;
+			x = CENTERED_X(DIALOG_W) + 1 * GRID_W;
+			y = DIALOG_TOP + 21 * GRID_H;
+			w = DIALOG_W * GRID_W - 4 * GRID_W;
+			h = 5 * GRID_H;
+		};
+		class IndexStartValue: ctrlEdit
+		{
+			idc = 1100;
+			text = "0";
+			x = CENTERED_X(DIALOG_W) + 2 * GRID_W;
+			y = DIALOG_TOP + 28 * GRID_H;
+			w = DIALOG_W * GRID_W - 40 * GRID_W;
+			h = 5 * GRID_H;
+		};
+		class OK: ctrlButtonOK
+		{
+			idc = -1;//We don't want it to close the dialog
+			x = CENTERED_X(DIALOG_W) + 2 * GRID_W;
+			y = DIALOG_TOP + 35 * GRID_H;
+			w = 27 * GRID_W;
+			h = 5 * GRID_H;
+			action = "call Enh_fnc_nameObjects_name";
+		};
+		class Cancel: ctrlButtonClose
+		{
+			x = CENTERED_X(DIALOG_W) + 41 * GRID_W;
+			y = DIALOG_TOP + 35 * GRID_H;
+			w = 27 * GRID_W;
+			h = 5 * GRID_H;
+		};
+		class RemoveNames: ctrlButton
+		{
+			text = $STR_ENH_nameObjects_removeName;
+			x = CENTERED_X(DIALOG_W) + 41 * GRID_W;
+			y = DIALOG_TOP + 7 * GRID_H;
+			w = 27 * GRID_W;
+			h = 5 * GRID_H;
+			action = "'UNNAME' call Enh_fnc_nameObjects_name";
+		};
+	};
+};
+
+/*class Enh_NameObjects
 {
 	idd = ENH_IDD_NAMEOBJECTS;
 	onLoad = "call Enh_fnc_nameObjects_onLoad";
