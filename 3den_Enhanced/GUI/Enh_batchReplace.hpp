8b7e084c87a8f0a89f6726c7dd90aa07be6c8d46
robertseibel@outlook.de <robertseibel@outlook.de>
2019-06-13 01:13:30 +0200

    Batch replace uses pixelGrid now

--- 3den_Enhanced/GUI/Enh_batchReplace.hpp
+++ 3den_Enhanced/GUI/Enh_batchReplace.hpp
@@ -1,3 +1,78 @@
+#define DIALOG_W 70
+#define DIALOG_H 45
+
+class Enh_BatchReplace
+{
+	idd = ENH_IDD_BATCHREPLACE;
+	movingEnable = true;
+	onLoad = "((_this # 0) displayCtrl 1400) ctrlSetText (profileNameSpace getVariable ['Enh_batchReplace_lastUsed','']); ctrlSetFocus ((_this # 0) displayCtrl 1400)";
+	onUnload = "profileNameSpace setVariable ['Enh_batchReplace_lastUsed',ctrlText ((_this # 0) displayCtrl 1400)]";
+	class ControlsBackground
+	{
+		DISABLE_BACKGROUND
+		class Background: ctrlStaticBackground
+		{
+			x = CENTERED_X(DIALOG_W);
+			y = DIALOG_TOP + 5 * GRID_H;
+			w = DIALOG_W * GRID_W;
+			h = DIALOG_H * GRID_H;
+		};
+	};
+	class Controls
+	{
+		class Header: ctrlStaticTitle
+		{
+			text = $STR_ENH_batchReplace_header;
+			x = CENTERED_X(DIALOG_W);
+			y = DIALOG_TOP;
+			w = DIALOG_W * GRID_W;
+			h = 5 * GRID_H;
+		};
+		class Description: ctrlStructuredText
+		{
+			text = $STR_ENH_batchReplace_description;
+			x = CENTERED_X(DIALOG_W);
+			y = DIALOG_TOP + 7 * GRID_H;
+			w = DIALOG_W * GRID_W;
+			h = 10 * GRID_H;
+		};
+		class ReplaceWithText: ctrlStatic
+		{
+			text = $STR_ENH_batchReplace_replaceWith;
+			x = CENTERED_X(DIALOG_W);
+			y = DIALOG_TOP + 19 * GRID_H;
+			w = DIALOG_W * GRID_W;
+			h = 5 * GRID_H;
+		};
+		class ReplaceWithValue: ctrlEditMulti
+		{
+			idc = 1400;
+			tooltip = $STR_ENH_batchReplace_replaceWith_tooltip;
+			x = CENTERED_X(DIALOG_W) + 19 * GRID_W;
+			y = DIALOG_TOP + 20 * GRID_H;
+			w = 49 * GRID_W;
+			h = 20 * GRID_H;
+		};
+		class Replace: ctrlButton
+		{
+			text = $STR_ENH_batchReplace_replace;
+			x = CENTERED_X(DIALOG_W) + 19 * GRID_W;
+			y = DIALOG_TOP + 42 * GRID_H;
+			w = 23 * GRID_W;
+			h = 5 * GRID_H;
+			action = "call Enh_fnc_batchreplace";
+		};
+		class Close: ctrlButtonCancel
+		{
+			x = CENTER_X + 10 * GRID_W;
+			y = DIALOG_TOP + 42 * GRID_H;
+			w = 23 * GRID_W;
+			h = 5 * GRID_H;
+		};
+	};
+};
+
+/*
 class Enh_BatchReplace
 {
 	idd = ENH_IDD_BATCHREPLACE;
