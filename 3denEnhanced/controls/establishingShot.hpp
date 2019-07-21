5d755a3905a2219500d69eb13ca6a1e8ee8a9073
robertseibel@outlook.de <robertseibel@outlook.de>
2019-07-21 13:07:21 +0200

    layout tweaks

--- 3denEnhanced/controls/establishingShot.hpp
+++ 3denEnhanced/controls/establishingShot.hpp
@@ -2,55 +2,87 @@ class Enh_EstablishingShot: Title
 {
 	attributeLoad = "[_this,_value] call Enh_fnc_establishingShot_onAttributeLoad";
 	attributeSave = "_this call Enh_fnc_establishingShot_onAttributeSave";
-	h = 8 * SIZE_M * GRID_H + 30 * pixelH;
+	h = 12.5 * SIZE_M * GRID_H + 35 * pixelH;
 	class Controls: Controls
 	{
-/* 		class ToggleOnOff: ctrlToolbox
-        {
-            idc = 100;
-			x = ATTRIBUTE_TITLE_W * GRID_W;
-			w = ATTRIBUTE_CONTENT_W * GRID_W;
-			h = SIZE_M * GRID_H;
-            rows = 1;
-            columns = 2;
-            strings[] = 
-            {
-				"Off", "On"
-            };
-            values[] = {0,1};
-			onToolBoxSelChanged = "_this call Enh_fnc_establishingShot_onToolBoxSelChanged";
-        }; */
+		class DescriptionGroup: ctrlControlsGroup
+		{
+			x = 5 * GRID_W;
+			y = 0.5 * SIZE_M * GRID_H;
+			h = 4 * SIZE_M * GRID_H;
+			w = (ATTRIBUTE_TITLE_W + ATTRIBUTE_CONTENT_W) * GRID_W;
+			class Controls
+			{
+				class DescriptionDeco: ctrlStatic
+				{
+					w = 5 * pixelW;
+					h = SIZE_M * GRID_H;
+					colorBackground[] = {1,1,1,0.05};
+				};
+				class DescriptionHeader: ctrlStatic
+				{
+					text = $STR_ENH_description;
+					x = 7 * pixelW;
+					w = (ATTRIBUTE_TITLE_W + ATTRIBUTE_CONTENT_W - 4) * GRID_W - 7 * pixelW;
+					h = SIZE_M * GRID_H;
+					colorText[] = {1,1,1,0.4};
+					colorBackground[] = {1,1,1,0.05};
+					colorShadow[] = {0,0,0,0};
+				};
+				class Description: ctrlStructuredText
+				{
+					text = $STR_ENH_establishingShot_description;
+					x = 7 * pixelW;
+					y = SIZE_M * GRID_H;
+					w = (ATTRIBUTE_TITLE_W + ATTRIBUTE_CONTENT_W - 4) * GRID_W - 7 * pixelW;
+					h = 3 * SIZE_M * GRID_H;
+					colorText[] = {1,1,1,0.4};
+					colorBackground[] = {1,1,1,0};
+					shadow = 0;
+					class Attributes
+					{
+						align = "left";
+						color = "#999999";
+						colorLink = "";
+						font = "RobotoCondensedLight";
+						size = 1;
+					};
+				};
+			};
+		};
 		class CenterTitle: Title
 		{
 			text = $STR_ENH_establishingShot_centerPos_displayName;
+			y = 4.5 * SIZE_M * GRID_H + 5 * pixelH;
 		};
 		class CenterValue: ctrlEdit
 		{
 			idc = 100;
 			x = ATTRIBUTE_TITLE_W * GRID_W;
+			y = 4.5 * SIZE_M * GRID_H + 5 * pixelH;
 			w = ATTRIBUTE_CONTENT_W * GRID_W;
 			h = SIZE_M * GRID_H;
 		};
 		class TextTitle: Title
 		{
-			y = SIZE_M * GRID_H + 5 * pixelH;
+			y = 5.5 * SIZE_M * GRID_H + 10 * pixelH;
 			text = $STR_ENH_establishingShot_displayedText;
 		};
 		class TextValue: CenterValue
 		{
 			idc = 101;
-			y = SIZE_M * GRID_H + 5 * pixelH;
+			y = 5.5 * SIZE_M * GRID_H + 10 * pixelH;
 		};
 		class AltitudeTitle: Title
 		{
-			y = 2 * SIZE_M * GRID_H + 10 * pixelH;
+			y = 6.5 * SIZE_M * GRID_H + 15 * pixelH;
 			text = $STR_ENH_altitude_displayName;
 		};
 		class AltitudeValue: ctrlXSliderH
 		{
 			idc = 102;
 			x = ATTRIBUTE_TITLE_W * GRID_W;
-			y = 2 * SIZE_M * GRID_H + 10 * pixelH;
+			y = 6.5 * SIZE_M * GRID_H + 15 * pixelH;
 			w = (ATTRIBUTE_CONTENT_W - EDIT_W_WIDE) * GRID_W;
 			h = SIZE_M * GRID_H;
 		 	sliderPosition = 500;
@@ -61,20 +93,20 @@ class Enh_EstablishingShot: Title
 		{
 			idc = 103;
 			x = (ATTRIBUTE_TITLE_W + ATTRIBUTE_CONTENT_W - EDIT_W_WIDE) * GRID_W;
-			y = 2 * SIZE_M * GRID_H + 10 * pixelH;
+			y = 6.5 * SIZE_M * GRID_H + 15 * pixelH;
 			w = EDIT_W_WIDE * GRID_W;
 			h = SIZE_M * GRID_H;
 		};
 		class RadiusTitle: Title
 		{
-			y = 3 * SIZE_M * GRID_H + 15 * pixelH;
+			y = 7.5 * SIZE_M * GRID_H + 20 * pixelH;
 			text = $STR_ENH_establishingShot_radius_displayName;
 		};
 		class RadiusValue: ctrlXSliderH
 		{
 			idc = 104;
 			x = ATTRIBUTE_TITLE_W * GRID_W;
-			y = 3 * SIZE_M * GRID_H + 15 * pixelH;
+			y = 7.5 * SIZE_M * GRID_H + 20 * pixelH;
 			w = (ATTRIBUTE_CONTENT_W - EDIT_W_WIDE) * GRID_W;
 			h = SIZE_M * GRID_H;
 		 	sliderPosition = 200;
@@ -85,20 +117,20 @@ class Enh_EstablishingShot: Title
 		{
 			idc = 105;
 			x = (ATTRIBUTE_TITLE_W + ATTRIBUTE_CONTENT_W - EDIT_W_WIDE) * GRID_W;
-			y = 3 * SIZE_M * GRID_H + 15 * pixelH;
+			y = 7.5 * SIZE_M * GRID_H + 20 * pixelH;
 			w = EDIT_W_WIDE * GRID_W;
 			h = SIZE_M * GRID_H;
 		};
 		class AngleTitle: Title
 		{
-			y = 4 * SIZE_M * GRID_H + 20 * pixelH;
+			y = 8.5 * SIZE_M * GRID_H + 25 * pixelH;
 			text = $STR_ENH_establishingShot_viewingAngle_displayName;
 		};
 		class AngleValue: ctrlXSliderH
 		{
 			idc = 106;
 			x = ATTRIBUTE_TITLE_W * GRID_W;
-			y = 4 * SIZE_M * GRID_H + 20 * pixelH;
+			y = 8.5 * SIZE_M * GRID_H + 25 * pixelH;
 			w = (ATTRIBUTE_CONTENT_W - EDIT_W_WIDE) * GRID_W;
 			h = SIZE_M * GRID_H;
 		 	sliderPosition = 45;
@@ -109,20 +141,20 @@ class Enh_EstablishingShot: Title
 		{
 			idc = 107;
 			x = (ATTRIBUTE_TITLE_W + ATTRIBUTE_CONTENT_W - EDIT_W_WIDE) * GRID_W;
-			y = 4 * SIZE_M * GRID_H + 20 * pixelH;
+			y = 8.5 * SIZE_M * GRID_H + 25 * pixelH;
 			w = EDIT_W_WIDE * GRID_W;
 			h = SIZE_M * GRID_H;
 		};
 		class DirectionTitle: Title
 		{
-			y = 5 * SIZE_M * GRID_H + 25 * pixelH;
+			y = 9.5 * SIZE_M * GRID_H + 30 * pixelH;
 			text = $STR_ENH_establishingShot_direction_displayName;
 		};
 		class DirectionValue: ctrlToolbox
         {
             idc = 108;
 			x = ATTRIBUTE_TITLE_W * GRID_W;
-			y = 5 * SIZE_M * GRID_H + 25 * pixelH;
+			y = 9.5 * SIZE_M * GRID_H + 30 * pixelH;
 			w = ATTRIBUTE_CONTENT_W * GRID_W;
 			h = 2 * SIZE_M * GRID_H;
             rows = 2;
@@ -137,9 +169,9 @@ class Enh_EstablishingShot: Title
 		class Reset: ctrlButton
 		{
 			x = ATTRIBUTE_TITLE_W * GRID_W;
+			y = 11.5 * SIZE_M * GRID_H + 35 * pixelH;
 			w = ATTRIBUTE_CONTENT_W * GRID_W;
 			h = SIZE_M * GRID_H;
-			y = 7 * SIZE_M * GRID_H + 30 * pixelH;
 			onButtonDown  = "_this call Enh_fnc_establishingShot_onButtonDown";
 			text = $STR_ENH_resetAndDisable;
 		};
