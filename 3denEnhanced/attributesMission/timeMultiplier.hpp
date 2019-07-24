45a58ed7dc97a1af084d86e90fbef48f5ce27ba4
robertseibel@outlook.de <robertseibel@outlook.de>
2019-07-24 13:15:02 +0200

    attribute is no longer executed with default value

--- 3denEnhanced/attributesMission/timeMultiplier.hpp
+++ 3denEnhanced/attributesMission/timeMultiplier.hpp
@@ -8,7 +8,7 @@ class Date
 			tooltip = $STR_ENH_timeMultiplier_tooltip;
 			property = "Enh_timeMultiplier";
 			control = "Enh_TimeMultiplier";
-			expression = "if (!is3DEN && isServer) then {setTimeMultiplier _value}";
+			expression = "if (!is3DEN && isServer && _value != 1) then {setTimeMultiplier _value}";
 			defaultValue = "1";
 		};
 	};
