df6cd80a677730bb97e44a6da2047fc097fa7864
robertseibel@outlook.de <robertseibel@outlook.de>
2019-08-10 10:13:26 +0200

    removed comment

--- 3denEnhanced/display3DEN.hpp
+++ 3denEnhanced/display3DEN.hpp
@@ -19,39 +19,5 @@ class Display3DEN
 				#include "menuStrip_help.hpp"
 			};
 		};
-		//Make WEST, EAST and GUER category collapsed by default DOESN'T WORK FOR GUER TV FOR UNKNOWN REASON
-	/* 	class PanelRight:ctrlControlsGroupNoScrollbars
-		{
-			class Controls
-			{
-				class PanelRightCreate: ctrlControlsGroupNoScrollbars
-				{
-					class Controls
-					{
-						class Create: ctrlControlsGroupNoScrollbars
-						{
-							class Controls
-							{
-                                class CreateObjectWEST: ctrlTree
-                                {
-                                    defaultItem[] = {};
-									//onLoad = "params ['_ctrl']; tvExpandAll _ctrl; tvCollapseAll _ctrl";
-                                };
-                                class CreateObjectEAST: CreateObjectWEST
-                                {
-                                    defaultItem[] = {};
-									//onLoad = "params ['_ctrl']; tvExpandAll _ctrl; tvCollapseAll _ctrl";
-                                };
-                                class CreateObjectGUER: CreateObjectEAST
-                                {
-                                    defaultItem[] = {};
-									//onLoad = "params ['_ctrl']; tvExpandAll _ctrl; tvCollapseAll _ctrl";
-                                };
-							};	
-						};
-					};
-				};
-			};
-		}; */
 	};
 };
