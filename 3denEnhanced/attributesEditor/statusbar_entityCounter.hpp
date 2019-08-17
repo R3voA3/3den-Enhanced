11420d5d0894b1b3a2f66dc866b8a21b3ae76bed
robertseibel@outlook.de <robertseibel@outlook.de>
2019-08-17 14:41:01 +0200

    fixed wrong paramter

--- 3denEnhanced/attributesEditor/statusbar_entityCounter.hpp
+++ 3denEnhanced/attributesEditor/statusbar_entityCounter.hpp
@@ -3,6 +3,6 @@ class Enh_Statusbar_EntityCounter
 	displayName = $STR_ENH_statusbar_EntityCounter_displayName;
 	property = "Enh_Statusbar_EntityCounter";
 	control = "Checkbox";
-	expression = "profileNamespace setVariable ['%s',_value]; 'INIT' call Enh_fnc_statusbar_entityCounter";
+	expression = "profileNamespace setVariable ['%s',_value]; 'init' call Enh_fnc_statusbar_entityCounter";
 	defaultValue = "profileNamespace getVariable ['Enh_Statusbar_EntityCounter',true]";
 };
