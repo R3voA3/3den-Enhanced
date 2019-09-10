c69c14b050986cc81a53869024a4a45a28d0e7ca
robertseibel@outlook.de <robertseibel@outlook.de>
2019-09-10 17:26:37 +0200

    changed translation strings

--- 3denEnhanced/GUI/garrison.hpp
+++ 3denEnhanced/GUI/garrison.hpp
@@ -20,7 +20,7 @@ class Enh_Garrison
 	{
 		class Header: ctrlStaticTitle
 		{
-			text = $STR_ENH_placementTools_garrison_header;
+			text = $STR_ENH_garrison_header;
 			x = CENTERED_X(DIALOG_W);
 			y = DIALOG_TOP;
 			w = DIALOG_W * GRID_W;
@@ -44,7 +44,7 @@ class Enh_Garrison
 		};
 		class Coverage: ctrlStatic
 		{
-			text = $STR_ENH_placementTools_coverage;
+			text = $STR_ENH_garrison_coverage;
 			x = CENTERED_X(DIALOG_W);
 			y = DIALOG_TOP + 12 * GRID_H;
 			w = 15 * GRID_W;
@@ -61,16 +61,16 @@ class Enh_Garrison
 			columns = 4;
 			strings[] = 
 			{
-				$STR_ENH_placementTools_10percent,
-				$STR_ENH_placementTools_25percent,
-				$STR_ENH_placementTools_50percent,
-				$STR_ENH_placementTools_100percent
+				$STR_ENH_garrison_10percent,
+				$STR_ENH_garrison_25percent,
+				$STR_ENH_garrison_50percent,
+				$STR_ENH_garrison_100percent
 			};
 			values[] = {10,4,2,1};
 		};
 		class Stance: ctrlStatic
 		{
-			text = $STR_ENH_placementTools_stance;
+			text = $STR_ENH_garrison_stance;
 			x = CENTERED_X(DIALOG_W);
 			y = DIALOG_TOP + 18 * GRID_H;
 			w = 15 * GRID_W;
@@ -87,9 +87,9 @@ class Enh_Garrison
 			columns = 3;
 			strings[] = 
 			{
-				$STR_ENH_placementTools_stance_auto,
-				$STR_ENH_placementTools_stance_crouch,
-				$STR_ENH_placementTools_stance_stand
+				$STR_ENH_garrison_stance_auto,
+				$STR_ENH_garrison_stance_crouch,
+				$STR_ENH_garrison_stance_stand
 			};
 			values[] = {0,1,2,3};
 		};
