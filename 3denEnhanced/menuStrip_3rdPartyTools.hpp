28f091784f3b985788286cdc88601ba2ba358269
robertseibel@outlook.de <robertseibel@outlook.de>
2020-01-17 11:48:44 +0100

    added 3rd party tools

---
+++ 3denEnhanced/menuStrip_3rdPartyTools.hpp
@@ -0,0 +1,71 @@
+class Enh_3rdPartyTools
+{
+	text = $STR_ENH_folder_3rdPartyTools;
+	items[] +=
+	{
+		"Enh_3rdPartyTools_ColorPicker",
+		"Enh_3rdPartyTools_ExtendedFunctionViewer",
+		"Enh_3rdPartyTools_DebugConsole",
+		"Enh_3rdPartyTools_EditingExtensions",
+		"Enh_3rdPartyTools_ZEI",
+		"Enh_3rdPartyTools_ZEC",
+		"Enh_3rdPartyTools_EdenExtendedObjects",
+		"Enh_3rdPartyTools_Emitter3Ditor",
+		"Enh_3rdPartyTools_CompoT",
+		"Enh_3rdPartyTools_DisableMissionPreviewShortcut"
+	};
+};
+class Enh_3rdPartyTools_ColorPicker
+{
+	text = "Color Picker by Connor";
+	picture = "\a3\3DEN\Data\Controls\ctrlMenu\link_ca.paa";
+	weblink = "https://steamcommunity.com/sharedfiles/filedetails/?id=1668729705";
+	opensNewWindow = 1;
+};
+class Enh_3rdPartyTools_ExtendedFunctionViewer: Enh_3rdPartyTools_ColorPicker
+{
+	text = "Extended Function Viewer by Connor";
+	picture = "\a3\3DEN\Data\Controls\ctrlMenu\link_ca.paa";
+	weblink = "https://steamcommunity.com/sharedfiles/filedetails/?id=1678581937";
+	opensNewWindow = 1;
+};
+class Enh_3rdPartyTools_DebugConsole: Enh_3rdPartyTools_ColorPicker
+{
+	text = "Debug Console by R3vo";
+	weblink = "https://steamcommunity.com/sharedfiles/filedetails/?id=1231625987";
+};
+class Enh_3rdPartyTools_EditingExtensions: Enh_3rdPartyTools_ColorPicker
+{
+	text = "Editing Extensions by 7erra";
+	weblink = "https://steamcommunity.com/sharedfiles/filedetails/?id=1387934106";
+};
+class Enh_3rdPartyTools_ZEI: Enh_3rdPartyTools_ColorPicker
+{
+	text = "ZEI - Zeus and Eden Interiors by LSD";
+	weblink = "https://steamcommunity.com/sharedfiles/filedetails/?id=1251859358";
+};
+class Enh_3rdPartyTools_ZEC: Enh_3rdPartyTools_ColorPicker
+{
+	text = "ZEC - Zeus and Eden Compositions by LSD";
+	weblink = "https://steamcommunity.com/sharedfiles/filedetails/?id=642912021&searchtext=ZEC";
+};
+class Enh_3rdPartyTools_EdenExtendedObjects: Enh_3rdPartyTools_ColorPicker
+{
+	text = "Eden Extended Objects by _SCAR";
+	weblink = "https://steamcommunity.com/sharedfiles/filedetails/?id=882231372";
+};
+class Enh_3rdPartyTools_Emitter3Ditor: Enh_3rdPartyTools_ColorPicker
+{
+	text = "Emitter 3Ditor by h-";
+	weblink = "https://steamcommunity.com/sharedfiles/filedetails/?id=1613905318";
+};
+class Enh_3rdPartyTools_CompoT: Enh_3rdPartyTools_ColorPicker
+{
+	text = "CompoT by mind";
+	weblink = "https://steamcommunity.com/sharedfiles/filedetails/?id=557183401";
+};
+class Enh_3rdPartyTools_DisableMissionPreviewShortcut: Enh_3rdPartyTools_ColorPicker
+{
+	text = "Disable 3den Mission Preview Shortcut by 7erra";
+	weblink = "https://steamcommunity.com/sharedfiles/filedetails/?id=1420348567";
+};
