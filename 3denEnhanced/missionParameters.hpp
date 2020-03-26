7d7b2b19fe3ee62e71391244506ecbae0fa8e8fc
robertseibel@outlook.de <robertseibel@outlook.de>
2020-03-26 17:32:34 +0100

    fixed an issue

--- 3denEnhanced/missionParameters.hpp
+++ 3denEnhanced/missionParameters.hpp
@@ -1,7 +1,7 @@
-//Include in description.ext
-//See https://community.bistudio.com/wiki/Arma_3_Mission_Parameters for the full documentation.
-class Params
-{
+//Include in description.ext 
+//See https://community.bistudio.com/wiki/Arma_3_Mission_Parameters for the full documentation. 
+class Params 
+{ 
 	#define COUNTDOWN_MIN 600
 	#define COUNTDOWN_MAX 3600
 	#define COUNTDOWN_DEFAULT -1
@@ -10,8 +10,8 @@ class Params
 	#define DAYTIMEHOUR_DEFAULT 19
 	#include "\a3\functions_f\Params\paramDaytimeHour.hpp"
 
-	#define DAYTIMEPERIOD_DEFAULT 12
-	#include "\a3\functions_f\Params\paramDaytimePeriod.hpp"
+	//#define DAYTIMEPERIOD_DEFAULT 12
+	//#include "\a3\functions_f\Params\paramDaytimePeriod.hpp"
 
 	#define DEBUGCONSOLE_DEFAULT 1
 	#include "\a3\functions_f\Params\paramDebugConsole.hpp"
@@ -20,11 +20,11 @@ class Params
 	#include "\a3\functions_f\Params\paramGuerFriendly.hpp"
 
 	#define TICKETS_MIN 100
-	#define TICKETS_MAX	1100
-	#define TICKETS_DEFAULT	-1
+	#define TICKETS_MAX 1100
+	#define TICKETS_DEFAULT -1
 	#include "\a3\functions_f\Params\paramRespawnTickets.hpp"
 
-	#define WEATHER_DEFAULT	40
+	#define WEATHER_DEFAULT 40
 	#include "\a3\functions_f\Params\paramWeather.hpp"
 
 	#define TIMEACCELERATION_DEFAULT 10
