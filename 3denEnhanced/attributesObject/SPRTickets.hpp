23abf0ccacddb44b590abeae11037b2cb43f7b5a
shukari <shukari@users.noreply.github.com>
2020-06-26 15:45:18 +0200

    fixed rpt error
    File 3denEnhanced\attributesObject\SPRTickets.hpp, line 6: '/Cfg3DEN/Object/AttributeCategories/StateSpecial/Attributes/ENH_SPR_Tickets.control': Missing ';' at the end of line

--- 3denEnhanced/attributesObject/SPRTickets.hpp
+++ 3denEnhanced/attributesObject/SPRTickets.hpp
@@ -3,7 +3,7 @@ class ENH_SPR_Tickets: ENH_SetCaptive
 	displayName = $STR_ENH_RESPAWNTICKETS_DISPLAYNAME;
 	tooltip = $STR_ENH_RESPAWNTICKETS_TOOLTIP;
 	property = "ENH_SPR_Tickets";
-	control = "EditShort"
+	control = "EditShort";
 	expression = "if (!is3DEN && {!isMultiplayer}) then {_this setVariable ['%s',_value]}";
 	typeName = "NUMBER";
 	defaultValue = "0";
