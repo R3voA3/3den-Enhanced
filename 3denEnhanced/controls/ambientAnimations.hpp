ab3376bd57412684bdacb158b0fb0985d0b0f230
R3vo <robertseibel(at)outlook.de>
2020-11-04 14:38:36 +0100

    ambient anim UI overhaul

--- 3denEnhanced/controls/ambientAnimations.hpp
+++ 3denEnhanced/controls/ambientAnimations.hpp
@@ -2,55 +2,52 @@ class ENH_AmbientAnimations: Title
 {
 	attributeLoad = "[_this,_value] call ENH_fnc_ambientAnimations_onAttributeLoad";
 	attributeSave = "_this call ENH_fnc_ambientAnimations_onAttributeSave";
-	h = 13.5 * SIZE_M * GRID_H + 25 * pixelH;
+	h = 4 * SIZE_M * GRID_H + 20 * pixelH;
 	class Controls: Controls
 	{
-		ATTRIBUTE_DESCRIPTION($STR_ENH_AMBIENTANIMATIONS_DESCRIPTION,1);
-		class AnimText: Title
+		class AnimTitle: Title
 		{
 			text = $STR_ENH_AMBIENTANIMATIONS_ANIMATION_DISPLAYNAME;
-			y = 2.5 * SIZE_M * GRID_H + 5 * pixelH;
 		};
-		class Anim: ctrlListbox
+		class Animation: ctrlCombo
 		{
 			idc = 100;
 			x = ATTRIBUTE_TITLE_W * GRID_W;
-			y = 2.5 * SIZE_M * GRID_H + 5 * pixelH;
 			w = ATTRIBUTE_CONTENT_W * GRID_W;
-			h = 8 * SIZE_M * GRID_H;
+			h = SIZE_M * GRID_H;
 		};
-		class CanExitTitle: Title 
+		class CanExitTitle: Title
 		{
 			text = $STR_ENH_AMBIENTANIMATIONS_CANEDIT_DISPLAYNAME;
 			tooltip = $STR_ENH_AMBIENTANIMATIONS_CANEDIT_TOOLTIP;
-			y = 10.5 * SIZE_M * GRID_H + 10 * pixelH;
+			y = SIZE_M * GRID_H + 5 * pixelH;
 		};
 		class CanExit: ctrlCheckbox
 		{
 			idc = 101;
 			x = ATTRIBUTE_TITLE_W * GRID_W;
-			y = 10.5 * SIZE_M * GRID_H + 10 * pixelH;
+			y = SIZE_M * GRID_H + 5 * pixelH;
 			w = 5 * GRID_W;
-			h = SIZE_M * GRID_H;
+	    h = SIZE_M * GRID_H;
 		};
 		class AttachTitle: Title 
 		{
 			text = $STR_ENH_AMBIENTANIMATIONS_ATTACH_DISPLAYNAME;
 			tooltip = $STR_ENH_AMBIENTANIMATIONS_ATTACH_TOOLTIP;
-			y = 11.5 * SIZE_M * GRID_H + 15 * pixelH;
+			y = 2 * SIZE_M * GRID_H + 10 * pixelH;
 		};
 		class Attach: ctrlCheckbox
 		{
 			idc = 102;
 			x = ATTRIBUTE_TITLE_W * GRID_W;
-			y = 11.5 * SIZE_M * GRID_H + 15 * pixelH;
+			y = 2 * SIZE_M * GRID_H + 10 * pixelH;
 			w = 5 * GRID_W;
 			h = SIZE_M * GRID_H;
 		};
 		class Reset: ctrlButton
 		{
 			x = ATTRIBUTE_TITLE_W * GRID_W;
-			y = 12.5 * SIZE_M * GRID_H + 20 * pixelH;
+			y = 3 * SIZE_M * GRID_H + 15 * pixelH;
 			w = ATTRIBUTE_CONTENT_W * GRID_W;
 			h = SIZE_M * GRID_H;
 			onButtonDown  = "_this call ENH_fnc_ambientAnimations_onButtonDown";
