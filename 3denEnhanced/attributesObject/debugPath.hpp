cefa2802a2fc6c5edada870a8c783bc99ed9474b
R3vo <robertseibel(at)outlook.de>
2020-11-10 17:33:59 +0100

    removed debug message and added tag to marker name

--- 3denEnhanced/attributesObject/debugPath.hpp
+++ 3denEnhanced/attributesObject/debugPath.hpp
@@ -10,18 +10,17 @@ class ENH_DebugPath
     [_this] spawn\
     {\
       scriptName 'ENH_Attribute_DebugPath';\
-      systemChat format [localize 'STR_ENH_DEBUGPATH_MESSAGE',str _this];\
       params ['_unit'];\
       private _arrow = objNull;\
       private _arrowColour = format ['#(rgb,8,8,3)color(%1,%2,%3,1)',random(1),random(1),random(1)];\
-      while {true} do\
+      while {alive _unit} do\
       {\
-        if ((_unit distance _arrow) > 20)  then\
+        if ((_unit distance _arrow) > 20) then\
         {\
           _arrow = createVehicle ['Sign_Arrow_Direction_Blue_F',position _unit,[],0,'CAN_COLLIDE'];\
           _arrow setObjectTexture [0,_arrowColour];\
           _arrow setDir getDir _unit;\
-          _markerName = format ['debugPath_%1_%2',str _unit,str random 100000];\
+          _markerName = format ['ENH_DebugPath_%1_%2',str _unit,str random 100000];\
           _marker = createMarker [_markerName,_unit];\
           _marker setMarkerDir getDir _unit;\
           _marker setMarkerType 'mil_arrow2';\
