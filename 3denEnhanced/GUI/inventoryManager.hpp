0e059cc7b342df384feab71b09eeb6e286f53e8d
R3vo <robertseibel(at)outlook.de>
2020-12-03 14:35:36 +0100

    merged into 2 files

--- 3denEnhanced/GUI/inventoryManager.hpp
+++ 3denEnhanced/GUI/inventoryManager.hpp
@@ -1,4 +1,3 @@
-#include "\3denEnhanced\defineCommon.hpp"
 #define DIALOG_W 240
 #define DIALOG_H 140
 
@@ -258,6 +257,121 @@ class ENH_InventoryManager
       y = DIALOG_TOP + DIALOG_H * GRID_H - GRID_H;
       w = 30 * GRID_W;
       h = CTRL_DEFAULT_H;
+    };
+  };
+};
+
+class ENH_InventoryManager_ItemPreview
+{
+  idd = IDD_INVENTORYMANAGER_ITEMPREVIEW;
+  class ControlsBackground
+  {
+    DISABLE_BACKGROUND
+    class Background: ctrlStaticBackground
+    {
+      x = CENTERED_X(60);
+      y = DIALOG_TOP + 50 * GRID_H + CTRL_DEFAULT_H;
+      w = 60 * GRID_W;
+      h = 60 * GRID_H;
+    };
+  };
+  class Controls
+  {
+    class Header: ctrlStaticTitle
+    {
+      idc = IDC_INVENTORYMANAGER_ITEMPREVIEW_HEADER;
+      x = CENTERED_X(60);
+      y = DIALOG_TOP + 50 * GRID_H;
+      w = 60 * GRID_W;
+      h = CTRL_DEFAULT_H;
+    };
+    class Image: ctrlStaticPictureKeepAspect
+    {
+      idc = IDC_INVENTORYMANAGER_ITEMPREVIEW_IMAGE;
+      x = CENTERED_X(60);
+      y = DIALOG_TOP + 50 * GRID_H + CTRL_DEFAULT_H;
+      w = 60 * GRID_W;
+      h = 60 * GRID_H;
+    };
+    class Close: ctrlButtonClose
+    {
+      x = CENTERED_X(60) + 35 * GRID_W;
+      y = DIALOG_TOP + 110 * GRID_H + CTRL_DEFAULT_H;
+      w = 25 * GRID_W;
+      h = CTRL_DEFAULT_H;
+    };
+  };
+};
+
+class ENH_InventoryManager_TemplateData
+{
+  idd = IDD_INVENTORYMANAGER_TEMPLATEDATA;
+  class ControlsBackground
+  {
+    DISABLE_BACKGROUND
+    class Background: ctrlStaticBackground
+    {
+      x = CENTERED_X(60);
+      y = DIALOG_TOP + 50 * GRID_H + CTRL_DEFAULT_H;
+      w = 60 * GRID_W;
+      h = 51 * GRID_H;
+    };
+  };
+  class Controls
+  {
+    class Header: ctrlStaticTitle
+    {
+      x = CENTERED_X(60);
+      y = DIALOG_TOP + 50 * GRID_H;
+      w = 60 * GRID_W;
+      h = CTRL_DEFAULT_H;
+    };
+    class Title: ctrlStatic
+    {
+      text = $STR_ENH_SCENARIOATTRIBUTESMANAGERTEMPLATEDATA_TITLE;
+      x = CENTERED_X(60);
+      y = DIALOG_TOP + 56 * GRID_H;
+      w = 58 * GRID_W;
+      h = CTRL_DEFAULT_H;
+    };
+    class TitleValue: ctrlEdit
+    {
+      idc = IDC_INVENTORYMANAGER_TEMPLATEDATA_TITLE;
+      x = CENTERED_X(60) + 1 * GRID_W;
+      y = DIALOG_TOP + 62 * GRID_H;
+      w = 58 * GRID_W;
+      h = CTRL_DEFAULT_H;
+    };
+    class Description: ctrlStatic
+    {
+      text = $STR_ENH_SCENARIOATTRIBUTESMANAGERTEMPLATEDATA_DESCRIPTION;
+      x = CENTERED_X(60);
+      y = DIALOG_TOP + 68 * GRID_H;
+      w = 58 * GRID_W;
+      h = CTRL_DEFAULT_H;
+    };
+    class DescriptionValue: ctrlEditMulti
+    {
+      idc = IDD_INVENTORYMANAGER_TEMPLATEDATA_DESCRIPTION;
+      x = CENTERED_X(60) + 1 * GRID_W;
+      y = DIALOG_TOP + 74 * GRID_H;
+      w = 58 * GRID_W;
+      h = 25 * GRID_H;
+    };
+    class Cancel: ctrlButtonCancel
+    {
+      x = CENTERED_X(60) + 34 * GRID_W;
+      y = DIALOG_TOP + 100 * GRID_H;
+      w = 25 * GRID_W;
+      h = CTRL_DEFAULT_H;
+    };
+    class OK: ctrlButtonOk
+    {
+      x = CENTERED_X(60) + 1 * GRID_W;
+      y = DIALOG_TOP + 100 * GRID_H;
+      w = 25 * GRID_W;
+      h = CTRL_DEFAULT_H;
+      onButtonClick = "_this call ENH_fnc_IM_createTemplate";
     };
   };
 };
