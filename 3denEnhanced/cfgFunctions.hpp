bf3e6b1a47393d05d3aae3c73b88ee0a92b545e1
R3vo <robertseibel(at)outlook.de>
2021-01-25 14:31:23 +0100

    cleanup and functions are sorted alphabetically

--- 3denEnhanced/cfgFunctions.hpp
+++ 3denEnhanced/cfgFunctions.hpp
@@ -17,103 +17,103 @@ class CfgFunctions
     class 3DENRadio
     {
       file = "3denEnhanced\functions\GUI\3DENRadio";
-      class 3DENRadio_onLoad;
       class 3DENRadio_exportClassname;
+      class 3DENRadio_handlePlaylist;
+      class 3DENRadio_onLoad;
+      class 3DENRadio_playNewSong;
+      class 3DENRadio_searchList;
       class 3DENRadio_timelineControl;
       class 3DENRadio_toggleRadio;
-      class 3DENRadio_searchList;
-      class 3DENRadio_playNewSong;
-      class 3DENRadio_handlePlaylist;
     };
     class Garrison
     {
       file = "3denEnhanced\functions\GUI\garrison";
-      class garrison_onLoad;
       class garrison_garrison;
+      class garrison_onLoad;
     };
     class Extraction
     {
       file = "3denEnhanced\functions\GUI\extraction";
-      class extraction_onLoad;
       class extraction_onCheckedChanged;
+      class extraction_onLoad;
       class extraction_setup;
     };
     class TextureFinder
     {
       file = "3denEnhanced\functions\GUI\TextureFinder";
-      class textureFinder_progressText;
-      class textureFinder_findTextures;
       class textureFinder_exportTexturePath;
       class textureFinder_fillList;
+      class textureFinder_findTextures;
+      class textureFinder_progressText;
     };
     class BatchReplace
     {
       file = "3denEnhanced\functions\GUI\batchReplace";
-      class batchReplace_createDisplay;
       class batchReplace;
+      class batchReplace_createDisplay;
     };
     class FunctionsViewer
     {
       file = "3denEnhanced\functions\GUI\functionsViewer";
+      class functionsViewer_copy;
+      class functionsViewer_fillCtrlTV;
+      class functionsViewer_getFunctionsData;
+      class functionsViewer_incrementKey;
       class functionsViewer_onLoad;
       class functionsViewer_onTreeSelChanged;
       class functionsViewer_recompileSelected;
-      class functionsViewer_copy;
-      class functionsViewer_getFunctionsData;
-      class functionsViewer_fillCtrlTV;
-      class functionsViewer_incrementKey;
       class functionsViewer_searchkey;
       class functionsViewer_togglePanel;
     };
     class ActionCreator
     {
       file = "3denEnhanced\functions\GUI\actionCreator";
-      class actionCreator_onLoad;
       class actionCreator_export;
+      class actionCreator_onLoad;
     };
     class BriefingEditor
     {
       file = "3denEnhanced\functions\GUI\briefingEditor";
-      class briefingEditor_onLoad;
-      class briefingEditor_onUnload;
-      class briefingEditor_export;
+      class briefingEditor_addTAG;
       class briefingEditor_createTemplate;
+      class briefingEditor_export;
       class briefingEditor_getTemplate;
       class briefingEditor_onLBSelChanged;
-      class briefingEditor_addTAG;
+      class briefingEditor_onLoad;
+      class briefingEditor_onUnload;
     };
     class PlacementTools
     {
       file = "3denEnhanced\functions\GUI\placementTools";
-      class placementTools_onLoad;
-      class placementTools_onUnload;
-      class placementTools_createDisplay;
       class placementTools_centralAngle;
+      class placementTools_createDisplay;
       class placementTools_fillArea;
       class placementTools_grid;
-      class placementTools_radius;
       class placementTools_initialAngle;
-      class placementTools_spacing;
+      class placementTools_onLoad;
       class placementTools_onToolBoxSelChanged;
+      class placementTools_onUnload;
+      class placementTools_radius;
+      class placementTools_spacing;
     };
     class ScenarioAttributesManager
     {
       file = "3denEnhanced\functions\GUI\scenarioAttributesManager";
-      class scenarioAttributesManager_onLoad;
-      class scenarioAttributesManager_onUnload;
-      class scenarioAttributesManager_loadTemplate;
-      class scenarioAttributesManager_deleteTemplate;
+      class scenarioAttributesManager_applyAttribute;
       class scenarioAttributesManager_applyTemplate;
       class scenarioAttributesManager_createTemplate;
-      class scenarioAttributesManager_applyAttribute;
-      class scenarioAttributesManager_getScenarioAttributes;
+      class scenarioAttributesManager_deleteTemplate;
       class scenarioAttributesManager_exportToClipboard;
+      class scenarioAttributesManager_getScenarioAttributes;
+      class scenarioAttributesManager_loadTemplate;
+      class scenarioAttributesManager_onLoad;
+      class scenarioAttributesManager_onUnload;
     };
     class NameObjects
     {
       file = "3denEnhanced\functions\GUI\nameObjects";
-      class nameObjects_onLoad;
       class nameObjects_name;
+      class nameObjects_onLoad;
     };
     class ModuleInformation
     {
@@ -123,32 +123,31 @@ class CfgFunctions
     class 3DENCam
     {
       file = "3denEnhanced\functions\GUI\3DENCam";
-      class 3DENCam_updateList;
       class 3DENCam_deleteEntry;
       class 3DENCam_get3DENCameraParameters;
-      class 3DENCam_set3DENCameraParameters;
       class 3DENCam_onKillFocus;
       class 3DENCam_onSetFocus;
       class 3DENCam_saveList;
+      class 3DENCam_set3DENCameraParameters;
+      class 3DENCam_updateList;
     };
     class VariableViewer
     {
       file = "3denEnhanced\functions\GUI\variableViewer";
-      class variableViewer_onLoad;
+      class variableViewer_delete;
       class variableViewer_fillLNB;
+      class variableViewer_getNamespace;
       class variableViewer_hideFunctions;
       class variableViewer_messageConfirm;
       class variableViewer_onFilterChanged;
       class variableViewer_onLNBSelChanged;
+      class variableViewer_onLoad;
       class variableViewer_onSearch;
       class variableViewer_setOrCreate;
-      class variableViewer_delete;
-      class variableViewer_getNamespace;
     };
     class IM
     {
       file = "3denEnhanced\functions\GUI\IM";
-      class IM_open;
       class IM_addItem;
       class IM_applyAttribute;
       class IM_applyTemplate;
@@ -159,72 +158,73 @@ class CfgFunctions
       class IM_lbAdd;
       class IM_lnbAdd;
       class IM_loadAttributeValue;
-      class IM_previewItem;
+      class IM_onModFilterChanged;
+      class IM_open;
       class IM_removeItem;
+      class IM_resetSearch;
+      class IM_resetStorage;
       class IM_search;
       class IM_showTemplates;
       class IM_toggleVirtual;
       class IM_updateTemplateList;
-      class IM_onModFilterChanged;
-      class IM_resetSearch;
-      class IM_resetStorage;
     };
     class CfgSentencesBrowser
     {
       file = "3denEnhanced\functions\GUI\CFGS";
-      class CFGS_onLoad;
-      class CFGS_changePage;
       class CFGS_changeFilter;
+      class CFGS_changePage;
       class CFGS_getCfgSentences;
       class CFGS_getSentenceDataFromConfig;
       class CFGS_handleFavorites;
+      class CFGS_onLoad;
       class CFGS_playOrCopy;
     };
     class Misc
     {
       file = "3denEnhanced\functions\Misc";
-      class sessionTimer;
-      class twoDigitsStr;
-      class loadoutTool;
-      class measureDistance;
-      class switchTime;
-      class toggleGrass;
+      class 3DENCollapseAssetBrowser;
+      class 3denNotificationNoSound;
+      class addCustomMenuStripEntries;
+      class all3DENSelected;
       class deleteCrew;
       class deleteEmptyLayers;
-      class removeGear;
-      class setOrientation;
-      class all3DENSelected;
-      class statusbar_entityCounter;
+      class dynamicViewDistance;
+      class floatToTime;
       class generateSteamMissionDescription;
-      class 3denNotificationNoSound;
-      class locationList;
+      class getAllItems;
+      class hasStorage;
       class initAmbientAnimationsInEditor;
+      class loadoutTool;
+      class locationList;
+      class measureDistance;
+      class menuStrip_removeItems;
+      class removeGear;
+      class sessionTimer;
+      class setOrientation;
+      class statusbar_entityCounter;
+      class switchTime;
+      class toggleAIFeatures;
+      class toggleAttribute;
       class toggleDrawBuildingPositions;
       class toggleDrawDLCIcons;
-      class toggleAttribute;
-      class toggleAIFeatures;
-      class floatToTime;
-      class 3DENCollapseAssetBrowser;
-      class hasStorage;
-      class dynamicViewDistance;
-      class addCustomMenuStripEntries;
-      class menuStrip_removeItems;
+      class toggleGrass;
+      class twoDigitsStr;
     };
     class Log
     {
       file = "3denEnhanced\functions\log";
-      class log;
-      class exportWithLB;
-      class exportLoadout;
       class exportGUIDefines;
+      class exportLoadout;
+      class exportWithLB;
+      class log;
     };
     class Debug
     {
       file = "3denEnhanced\functions\debug";
+      class activeScripts;
+      class debugOptions;
       class productInfo;
       class toggleMapIDs;
-      class debugOptions;
-      class activeScripts;
     };
     class EventHandlers
     {
@@ -239,32 +239,32 @@ class CfgFunctions
     class AttributeControls
     {
       file = "3denEnhanced\functions\attributeControls";
-      class groupMarker_onAttributeLoad;
-      class groupMarker_onAttributeSave;
-      class ambientAnimations_onAttributeLoad;
-      class ambientAnimations_onAttributeSave;
-      class establishingShot_onAttributeLoad;
-      class establishingShot_onAttributeSave;
-      class holdAction_onAttributeLoad;
-      class holdAction_onAttributeSave;
-      class introText_onAttributeLoad;
-      class introText_onAttributeSave;
-      class timeMultiplier_onAttributeLoad;
-      class timeMultiplier_onAttributeSave;
+      class SPR_onAttributeLoad;
+      class SPR_onAttributeSave;
       class airdrop_onAttributeLoad;
       class airdrop_onAttributeSave;
+      class ambientAnimations_onAttributeLoad;
+      class ambientAnimations_onAttributeSave;
       class ambientFlyby_onAttributeLoad;
       class ambientFlyby_onAttributeSave;
       class dynamicSkill_onAttributeLoad;
       class dynamicSkill_onAttributeSave;
+      class establishingShot_onAttributeLoad;
+      class establishingShot_onAttributeSave;
+      class groupMarker_onAttributeLoad;
+      class groupMarker_onAttributeSave;
+      class holdAction_onAttributeLoad;
+      class holdAction_onAttributeSave;
+      class initDebriefingCombo;
+      class introText_onAttributeLoad;
+      class introText_onAttributeSave;
       class mapIndicators_onAttributeLoad;
       class mapIndicators_onAttributeSave;
+      class menuStripBlacklist_onAttributeLoad;
       class missionEndingCasualties_onAttributeLoad;
       class missionEndingCasualties_onAttributeSave;
-      class SPR_onAttributeLoad;
-      class SPR_onAttributeSave;
-      class initDebriefingCombo;
-      class menuStripBlacklist_onAttributeLoad;
+      class timeMultiplier_onAttributeLoad;
+      class timeMultiplier_onAttributeSave;
     };
   };
 };
