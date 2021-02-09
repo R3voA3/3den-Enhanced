0161ff8f3ef896b6be89645311f33cc32bb5ee84
R3vo <robertseibel(at)outlook.de>
2021-02-09 12:52:58 +0100

    removed not needed properties

--- 3denEnhanced/cfgPatches.hpp
+++ 3denEnhanced/cfgPatches.hpp
@@ -2,13 +2,9 @@ class CfgPatches
 {
   class 3denEnhanced
   {
-    name = "3den Enhanced";
     author = "R3vo";
-    url = "https://steamcommunity.com/sharedfiles/filedetails/?id=623475643";
-    requiredVersion = 2.0;
+    name = "3den Enhanced";
     requiredAddons[] = {"3den", "A3_Data_F_Sams_Loadorder"};
-    units[] = {};
-    weapons[] = {};
-    worlds[] = {};
+    url = "https://steamcommunity.com/sharedfiles/filedetails/?id=623475643";
   };
 };
