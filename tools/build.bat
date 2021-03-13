10b362d100be4bb7a236d867561b3a140fcff457
R3vo <robertseibel(at)outlook.de>
2021-03-13 13:01:19 +0100

    update

--- tools/build.bat
+++ tools/build.bat
@@ -1,5 +1,5 @@
 :: Turn of messages from commands
-@ECHO off
+:: @ECHO off
 
 :: Ask user for version
 SET /P VERSION_MAJOR=Major Version?
@@ -26,18 +26,22 @@ COPY "%PROJECT_FOLDER%\keys\3denEnhanced.bikey" "%TARGET_FOLDER%\keys\3denEnhanc
 COPY "%PROJECT_FOLDER%\userconfig\ENH_Config.hpp" "%TARGET_FOLDER%\userconfig\ENH_Config.hpp"
 
 :: Change dir
-CD C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\AddonBuilder\
-::Start AddonBuilder and pack mod
-START AddonBuilder.exe "%PROJECT_FOLDER%\3denEnhanced" "%TARGET_FOLDER%\addons" -packonly -sign="%PROJECT_FOLDER%\3denEnhanced.biprivatekey"
+CD "C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\AddonBuilder\"
+::Start AddonBuilder and pack mod and wait until it's done
+START /WAIT AddonBuilder.exe "%PROJECT_FOLDER%\3denEnhanced" "%TARGET_FOLDER%\addons" -packonly -sign="%PROJECT_FOLDER%\3denEnhanced.biprivatekey"
 
 :: Pack everything into .zip file
 ECHO .zip created and moved to OneDrive.
 POWERSHELL compress-archive -Path """%TARGET_FOLDER%""" -DestinationPath """%PROJECT_FOLDER%\versions\@3den Enhanced v%VERSION_MAJOR%.%VERSION_MINOR%.zip""" -force
 
+:: Update Steam Version via Publisher
+:: Change dir
+CD "C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\Publisher"
+::Start AddonBuilder and pack mod
+PublisherCmd update /id:2422196882 /changeNote:"Auto Update: Changelog will follow!" /path:"%TARGET_FOLDER%"
+
 :: Clear AddonBuilder log folder
 ECHO Clearing log files of AddonBuilder.
 DEL "C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\Logs" /Q
-
-:: TODO: Automate publishing to Steam
 
 PAUSE
