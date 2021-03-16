a4a82724f0b1fceabf54e6ee5ca44c2f4a379733
R3vo <robertseibel(at)outlook.de>
2021-03-16 17:58:56 +0100

    put name on top to prevent issue with Arma 3 Launcher

--- mod.cpp
+++ mod.cpp
@@ -1,3 +1,4 @@
+name = "3den Enhanced";
 action = "http://steamcommunity.com/sharedfiles/filedetails/?id=623475643";
 actionName = "Workshop";
 author = "R3vo";
@@ -6,7 +7,6 @@ hidePicture = 0;
 logo = "\3denEnhanced\data\logo.paa";
 logoOver = "\3denEnhanced\data\logoOver.paa";
 logoSmall = "\3denEnhanced\data\logoSmall.paa";
-name = "3den Enhanced";
 overview = "Adds new functionalities to the Eden Editor. Mod is not needed for the mission to be played. No dependencies are created.<br/><a href='https://www.bohemia.net/community/licenses/arma-public-license-share-alike'><t underline='true'>This content is under APL-SA license.</t><br/></a><br/><t size='1.5'><img image='\3denEnhanced\data\APL-SA_Custom.paa' /></t><br/><br/><t size='1.1' color='#11115'>Re-uploading to the Steam Workshop is prohibited!</t>";
 picture = "\3denEnhanced\data\picture.paa";
 tooltipOwned = "Adds new functionalities to the Eden Editor. Mod is not needed for the mission to be played. No dependencies are created.";
