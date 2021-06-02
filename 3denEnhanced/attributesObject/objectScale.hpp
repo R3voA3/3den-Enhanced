6b6981d9bfc81043365f4b57c93b011679ad6345
R3vo <robertseibel(at)outlook.de>
2021-06-02 10:16:02 +0200

    object scaling attribute

--- 3denEnhanced/attributesObject/objectScale.hpp
+++ 3denEnhanced/attributesObject/objectScale.hpp
@@ -1,22 +1,24 @@
-class ENH_ObjectScale
+class ENH_ObjectScaling
 {
-  displayName = "EDITOR ONLY! Object Scale";
-  tooltip = "EDITOR ONLY! Scales the size of the object. Object scaling is only persistent on simple Objects and/or objects where simulation is disabled.\nGenerally speaking, object scaling is not officially supported by Arma 3 and may cause unexpected behaviour.\nValue is limited from 0.0001 to 65504.";
+  displayName = "$STR_ENH_OBJECTSCALING_DISPLAYNAME";
   property = "ENH_objectScaling";
-  control = "EditShort";
-  expression = "if (!is3DEN) exitWith {};\
-  _this addEventHandler ['AttributesChanged3DEN',\
+  control = "ENH_ObjectScaling";
+  expression = "\
+  if (is3DEN) then\
   {\
-    params ['_object'];\
-    _object setObjectScale ((_object get3DENAttribute 'ENH_objectScaling') # 0);\
-  }];\
-  _this addEventHandler ['Dragged3DEN',\
-  {\
-    params ['_object'];\
-    _object setObjectScale ((_object get3DENAttribute 'ENH_objectScaling') # 0);\
-  }];\
-  _object setObjectScale _value";
-  condition = "1";
+    _this addEventHandler ['AttributesChanged3DEN',\
+    {\
+      params ['_object'];\
+      _object setObjectScale ((_object get3DENAttribute 'ENH_objectScaling') # 0);\
+    }];\
+    _this addEventHandler ['Dragged3DEN',\
+    {\
+      params ['_object'];\
+      _object setObjectScale ((_object get3DENAttribute 'ENH_objectScaling') # 0);\
+    }];\
+  };\
+  [_this, _value] spawn {sleep 0.01; (_this select 0) setObjectScale (_this select 1)}";
+  condition = "1 - objectSimulated";
   defaultValue = "1";
   typeName = "NUMBER";
   validate = "NUMBER";
---
+++ 3denEnhanced/controls/objectScaling.hpp
@@ -0,0 +1,26 @@
+class ENH_ObjectScaling: Title
+{
+  attributeLoad = "[_this, _value] call ENH_fnc_slider_onAttributeLoad";
+  attributeSave = "_this call ENH_fnc_slider_onAttributeSave";
+  class Controls: Controls
+  {
+    class Title: Title {};
+    class Value: ctrlXSliderH
+    {
+      idc = 100;
+      x = ATTRIBUTE_TITLE_W * GRID_W;
+      w = (ATTRIBUTE_CONTENT_W - EDIT_W) * GRID_W;
+      h = CTRL_DEFAULT_H;
+      sliderPosition = 1;
+      sliderRange[] = {0.1, 100};
+      sliderStep = 0.1;
+    };
+    class Edit: ctrlEdit
+    {
+      idc = 101;
+      x = (ATTRIBUTE_TITLE_W + ATTRIBUTE_CONTENT_W - EDIT_W) * GRID_W;
+      w = EDIT_W * GRID_W;
+      h = CTRL_DEFAULT_H;
+    };
+  };
+};
