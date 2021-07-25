969b8bcc92133e570579620ba9a0ad26e15a727e
R3vo <robertseibel(at)outlook.de>
2021-07-25 21:56:46 +0200

    tool update

--- tools/xmlToExcel.ps1
+++ tools/xmlToExcel.ps1
@@ -1,9 +1,11 @@
-
 # Force unicode for all cmdlets
 $PSDefaultParameterValues['*:Encoding'] = 'utf8'
 
+$PathXML = $env:OneDrive + "\Games\Arma 3\Arma 3 Mods\3den-Enhanced\3denEnhanced\stringtable.xml"
+$PathXLSX = $env:OneDrive + "\Games\Arma 3\Arma 3 Mods\3den-Enhanced\stringtable.xlsx"
+
 # Get XML content
-[xml] $XML = Get-Content -Path ($env:OneDrive + "\Games\Arma 3\Arma 3 Mods\3den-Enhanced\3denEnhanced\stringtable.xml")
+[xml] $XML = Get-Content -Path $PathXML
 
 # Create Excel file
 $Excel = New-Object -ComObject Excel.Application
@@ -20,120 +22,137 @@ $Worksheet.Name = "Translations"
 
 # Define the headers
 $Headers = @(
-    "Key",
-    "English",
-    "German",
-    "Italian",
-    "Czech",
-    "Chinesesimp",
-    "Spanish",
-    "French",
-    "Polish",
-    "Russian",
-    "Portuguese"
+  "Key",
+  "English",
+  "German",
+  "Italian",
+  "Czech",
+  "Chinesesimp",
+  "Spanish",
+  "French",
+  "Polish",
+  "Russian",
+  "Portuguese"
 )
-
-
 
 # Loop through the headers and create them
 $Col = 1
-$Headers | ForEach-Object -Process {
-    $Worksheet.Cells.Item(1,$col) = $_
-    $Worksheet.Cells.Item(1,$col).Font.Bold = $True
-    $Worksheet.Cells.Item(1,$col).Font.Size = 18
-    $Col++
+$row = 1
+
+$Headers | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $col) = $_
+  $Worksheet.Cells.Item($row, $col).Font.Bold = $True
+  $Worksheet.Cells.Item($row, $col).Font.Size = 18
+  $Col++
 }
 
-
+$Col = 1
 $row = 2
-$XML.Project.ChildNodes.ID | ForEach-Object -Process {
-    $Worksheet.Cells.Item($row,1) = $_
-    $row++
+$XML.Project.ChildNodes.ID | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $Col) = $_
+  $row++
 };
 
-$row = 2
 $Col = 2
-$XML.Project.ChildNodes.English | ForEach-Object -Process {
-    $Worksheet.Cells.Item($row, $Col) = $_
+$row = 2
+$XML.Project.ChildNodes.English | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $Col) = $_
 
-    $row++
+  $row++
 };
 
-$row = 2
 $Col = 3
-$XML.Project.ChildNodes.German | ForEach-Object -Process {
-    $Worksheet.Cells.Item($row, $Col) = $_
+$row = 2
+$XML.Project.ChildNodes.German | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $Col) = $_
 
-    $row++
+  $row++
 };
 
-$row = 2
 $Col = 4
-$XML.Project.ChildNodes.Italian | ForEach-Object -Process {
-    $Worksheet.Cells.Item($row, $Col) = $_
+$row = 2
+$XML.Project.ChildNodes.Italian | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $Col) = $_
 
-    $row++
+  $row++
 };
 
-$row = 2
 $Col = 5
-$XML.Project.ChildNodes.Czech | ForEach-Object -Process {
-    $Worksheet.Cells.Item($row, $Col) = $_
+$row = 2
+$XML.Project.ChildNodes.Czech | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $Col) = $_
 
-    $row++
+  $row++
 };
 
-$row = 2
 $Col = 6
-$XML.Project.ChildNodes.Chinesesimp | ForEach-Object -Process {
-    $Worksheet.Cells.Item($row, $Col) = $_
+$row = 2
+$XML.Project.ChildNodes.Chinesesimp | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $Col) = $_
 
-    $row++
+  $row++
 };
 
-$row = 2
 $Col = 7
-$XML.Project.ChildNodes.Spanish | ForEach-Object -Process {
-    $Worksheet.Cells.Item($row, $Col) = $_
+$row = 2
+$XML.Project.ChildNodes.Spanish | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $Col) = $_
 
-    $row++
+  $row++
 };
 
-$row = 2
 $Col = 8
-$XML.Project.ChildNodes.French | ForEach-Object -Process {
-    $Worksheet.Cells.Item($row, $Col) = $_
+$row = 2
+$XML.Project.ChildNodes.French | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $Col) = $_
 
-    $row++
+  $row++
 };
 
-$row = 2
 $Col = 9
-$XML.Project.ChildNodes.Polish | ForEach-Object -Process {
-    $Worksheet.Cells.Item($row, $Col) = $_
+$row = 2
+$XML.Project.ChildNodes.Polish | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $Col) = $_
 
-    $row++
+  $row++
 };
 
-$row = 2
 $Col = 10
-$XML.Project.ChildNodes.Russian | ForEach-Object -Process {
-    $Worksheet.Cells.Item($row, $Col) = $_
+$row = 2
+$XML.Project.ChildNodes.Russian | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $Col) = $_
 
-    $row++
+  $row++
 };
 
-$row = 2
 $Col = 11
-$XML.Project.ChildNodes.Portuguese | ForEach-Object -Process {
-    $Worksheet.Cells.Item($row, $Col) = $_
+$row = 2
+$XML.Project.ChildNodes.Portuguese | ForEach-Object -Process
+{
+  $Worksheet.Cells.Item($row, $Col) = $_
 };
 
 # Auto size all columns
 $Worksheet.Columns.autofit()
 
-$Workbook.SaveAs(($env:OneDrive + "\Games\Arma 3\Arma 3 Mods\3den-Enhanced\stringtable"), 51) # http://msdn.microsoft.com/en-us/library/bb241279.aspx
-$workbookObject.Close()
-$Workbook.Close()
+# Check if file already exists, if yes, delete it
+if (Test-Path $PathXLSX)
+{
+  Remove-Item -Path $PathXLSX
+}
 
-Get-Process -Name "Excel" | Stop-Process
+$excel.DisplayAlerts = 'False'
+$Workbook.SaveAs($PathXLSX, 51) # http://msdn.microsoft.com/en-us/library/bb241279.aspx
+$Workbook.Close()
+$excel.Quit()
