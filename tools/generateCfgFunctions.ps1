6a3399317d792e74d2a73ba6a9afcc9f58fe3690
R3vo <robertseibel(at)outlook.de>
2021-08-14 18:45:27 +0200

    updated tool

--- tools/generateCfgFunctions.ps1
+++ tools/generateCfgFunctions.ps1
@@ -1,4 +1,20 @@
-$Items = Get-ChildItem -Path "C:\Users\rober\OneDrive\Dokumente\Arma 3 - Other Profiles\R3vo\missions\TESTING\scriptLibrary.VR\Functions\ScriptLibrary" -Name
+Function Get-Folder
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $foldername.Description = "Select a folder"
+    $foldername.rootfolder = "Desktop"
+    $foldername.SelectedPath = "MyDocuments"
+    $folderName.ShowNewFolderButton = $false
+
+    if($foldername.ShowDialog() -eq "OK")
+    {
+        $folder += $foldername.SelectedPath
+    }
+    return $folder
+}
+
 Function Print-Line
 {
     param([INT] $Indentations = 0, [STRING] $Text = "line")
@@ -10,17 +26,21 @@ Function Print-Line
     $OutPut + $Text
 }
 
+$directory = Get-Folder
+$Items = Get-ChildItem -Path $directory -Name
+
 Print-Line "0" "CfgFunctions"
 Print-Line "0" "{"
 Print-Line "2" "class ENH"
 Print-Line "2" "{"
 Print-Line "4" "class Category"
 Print-Line "4" "{"
+
 $Items | forEach-Object {
-    $FunctionName = [STRING] $_.Replace("fn_", "      class ")
-    $FunctionName.Replace(".sqf", ";")
+    $FunctionName = [STRING] $_.Replace("fn_", "class ")
+    Print-Line "6" $FunctionName.Replace(".sqf", ";")
 }
-Print-Line "6" "}"
+
 Print-Line "4" "}"
 Print-Line "2" "}"
 Print-Line "0" "}"
