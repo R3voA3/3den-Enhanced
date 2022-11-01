6065197904d5970063ae39ba21fe663eef69afa7
R3voA3 <robertseibel@outlook.de>
2022-11-01 18:07:58 +0100

    version push

--- tools/build.ps1
+++ tools/build.ps1
@@ -1,7 +1,7 @@
 param($DoPublish = "false")
 
 $ProjectFolder = "$env:OneDrive\Games\Arma 3\Arma 3 Mods\3den-Enhanced"
-$ModVersion = "7.6.4" #Get-Content -Path "$ProjectFolder\tools\`$VERSION`$"
+$ModVersion = "7.6.5" #Get-Content -Path "$ProjectFolder\tools\`$VERSION`$"
 $TargetFolder = "${env:ProgramFiles(x86)}\Steam\steamapps\common\Arma 3\Mods\@3den Enhanced v$ModVersion"
 $ToolsFolder = "${env:ProgramFiles(x86)}\Steam\steamapps\common\Arma 3 Tools"
 $WorkshopID = 623475643
