05a7241a7fa382a051136217a94aefaa3d2413ba
R3voA3 <robertseibel@outlook.de>
2022-11-20 16:16:43 +0100

    - new code for save loadout

--- 3denEnhanced/attributesMission/saveLoadout.hpp
+++ 3denEnhanced/attributesMission/saveLoadout.hpp
@@ -1,4 +1,4 @@
-class ENH_SaveLoadout
+/* class ENH_SaveLoadout
 {
   displayName = "$STR_ENH_SAVELOADOUT_DISPLAYNAME";
   tooltip = "$STR_ENH_SAVELOADOUT_TOOLTIP";
@@ -17,5 +17,35 @@ class ENH_SaveLoadout
       };\
     };\
   ";
+  defaultValue = "false";
+}; */
+
+class ENH_SaveLoadout
+{
+  displayName = "$STR_ENH_SAVELOADOUT_DISPLAYNAME";
+  tooltip = "$STR_ENH_SAVELOADOUT_TOOLTIP";
+  property = "ENH_saveLoadout";
+  control = "Checkbox";
+  expression =
+  "if (!isMultiplayer || !_value || is3DEN) exitWith {};\
+    if (hasInterface) then\
+    {\
+      [] spawn\
+      {\
+        waitUntil {sleep 1; !isNull player};\
+        diag_log format ['ENH_saveLoadout: Player (%1) was detected at %2.', player, time];\
+        player setVariable ['ENH_savedLoadout', getUnitloadout player];\
+        player addMPEventHandler ['MPRespawn',\
+        {\
+          params ['_unit', '_corpse'];\
+          private _loadout = _unit getVariable ['ENH_savedLoadout', []];\
+          if (_loadout isNotEqualTo []) then\
+          {\
+            _unit setUnitLoadout _loadout;\
+            diag_log format ['ENH_saveLoadout: Loadout of player (%1) was set to %2 at %3', _unit, _loadout, time];\
+          };\
+        }];\
+      };\
+    };";
   defaultValue = "false";
 };
