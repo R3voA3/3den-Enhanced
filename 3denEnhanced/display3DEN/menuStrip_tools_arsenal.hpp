5e36d4ed62adbc6c5a1047432a2b186e6edbb2fa
Patrick Orave <oravep@gmail.com>
2023-03-26 14:25:40 -0500

    create menustrip hpp

---
+++ 3denEnhanced/display3DEN/menuStrip_tools_arsenal.hpp
@@ -0,0 +1,19 @@
+class ENH_Folder_Arsenal {
+	text = "$STR_ENH_FOLDER_ARSENAL";
+	picture = "\a3\3DEN\Data\Displays\Display3DEN\ToolBar\open_ca.paa";
+	items[] +=
+	{
+		"ENH_Init_Arsenal",
+		"ENH_Limit_Arsenal",
+		"ENH_Remove_Arsenal"
+	};
+};
+class ENH_Init_Arsenal {
+	text = "$STR_ENH_TOOLS_INIT_ARSENAL";
+}
+class ENH_Limit_Arsenal {
+	text = "$STR_ENH_TOOLS_LIMIT_ARSENAL";
+}
+class ENH_Remove_Arsenal {
+	text = "$STR_ENH_TOOLS_REMOVE_ARSENAL";
+}
