248121260d6ca092aaead63f4a8798f81bba53fa
R3vo <robertseibel@outlook.de>
2023-04-03 19:05:56 +0200

    Update menuStrip_tools_arsenal.hpp

--- 3denEnhanced/display3DEN/menuStrip_tools_arsenal.hpp
+++ 3denEnhanced/display3DEN/menuStrip_tools_arsenal.hpp
@@ -1,13 +1,15 @@
-class ENH_Folder_Arsenal {
+class ENH_Folder_Arsenal 
+{
 	text = "$STR_ENH_FOLDER_ARSENAL";
 	picture = "\a3\3DEN\Data\Displays\Display3DEN\ToolBar\open_ca.paa";
 	items[] +=
 	{
-		"ENH_ARSENAL"
+		"ENH_Arsenal"
 	};
 };
-class ENH_ARSENAL {
+class ENH_Arsenal 
+{
 	text = "$STR_ENH_TOOLS_LIMIT_ARSENAL";
 	action = "call ENH_fnc_VAM_arsenalPrototype";
 	opensNewWindow = 1;
-}
+};
