5c31496368fcf567b1f6380eb976de73220b04da
R3vo <robertseibel@outlook.de>
2023-04-03 19:04:59 +0200

    Update cfgFunctions.hpp

--- 3denEnhanced/cfgFunctions.hpp
+++ 3denEnhanced/cfgFunctions.hpp
@@ -175,7 +175,7 @@ class CfgFunctions
       class VIM_sort;
       class VIM_toggleVirtual;
     };
-    class virtualArsenalManager
+    class VirtualArsenalManager
     {
       file = "3denEnhanced\functions\GUI\virtualArsenalManager";
       class VAM_arsenalPrototype;
