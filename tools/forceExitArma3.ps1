5b6b989fbabbc514a5c9af610f66f3b7f0bd58bc
R3voArma3 <robertseibel@outlook.dem>
2023-04-24 20:39:06 +0200

    - removed obsolete tools

--- tools/forceExitArma3.ps1
+++
@@ -1,2 +0,0 @@
-#Force exit. If process does not exist, exit silently
-Get-Process -Name arma3_x64 -ErrorAction SilentlyContinue | Stop-Process
--- tools/readRPT.ps1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem "$env:USERPROFILE\\AppData\\Local\\Arma 3" -Filter *.rpt |
-Sort-Object LastAccessTime -descending |
-Select-Object -first 1 |
-Get-Content -tail 1 -wait -encoding utf8
