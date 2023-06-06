ac02d474c48bc03404a49d5d7c63776b401a2edd
R3voArma3 <robertseibel@outlook.dem>
2023-06-06 19:59:42 +0200

    - added debug function

--- 3denEnhanced/cfgFunctions.hpp
+++ 3denEnhanced/cfgFunctions.hpp
@@ -303,6 +303,7 @@ class CfgFunctions
       class checkShortCutsDuplicates;
       class exportAttributesToGitHub;
       class exportMenuStripToGitHub;
+      class testDisplays;
     };
   };
 };
