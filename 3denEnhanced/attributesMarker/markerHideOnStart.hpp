284a7b3a2b7cd605c052c6c17c63d1bf556fe289
R3voArma3 <robertseibel@outlook.dem>
2023-09-09 13:19:27 +0200

    - updated marker attributes

--- 3denEnhanced/attributesMarker/markerHideOnStart.hpp
+++ 3denEnhanced/attributesMarker/markerHideOnStart.hpp
@@ -8,37 +8,29 @@ class ENH_MarkerHideOnStart
       displayName = "$STR_ENH_MARKERHIDEONSTART_DISPLAYNAME";
       tooltip = "$STR_ENH_MARKERHIDEONSTART_TOOLTIP";
       property = "ENH_markerHideOnStart";
-      control = "CheckboxNumber";
+      control = "Checkbox";
       expression = "\
-        if (!isServer) exitWith {};\
-        if (is3DEN) exitWith {};\
-        if (_value >= 0) then {_this setMarkerAlpha 0};\
-      ";
-      defaultValue = 0;
-      typeName = "NUMBER";
+        if (!isServer || is3DEN || !_value) exitWith {};\
+        _this setMarkerAlpha 0";
+      defaultValue = "false";
     };
     class ENH_MarkerShowOnCondition
     {
       displayName = "$STR_ENH_MARKERSHOWONCONDITION_DISPLAYNAME";
       tooltip = "$STR_ENH_MARKERSHOWONCONDITION_TOOLTIP";
       property = "ENH_markerShowOnCondition";
-      control = "EditCode";
+      control = "EditCodeMulti3";
       expression = "\
-        if (!isServer) exitWith {};\
-        if (is3DEN) exitWith {};\
-        if (_value == 'false' || _value == '') exitWith {};\
-        [_this, _value] spawn {\
-          params ['_marker', '_value'];\
+        if (!isServer || is3DEN || _value == 'false' || _value == '') exitWith {};\
+        [_this, compile _value] spawn\
+        {\
+          params ['_marker', '_compiledValue'];\
           scriptName 'ENH_MarkerShowOnCondition';\
-          waitUntil {\
-            sleep 0.5;\
-            call compile _value;\
-          };\
+          waitUntil {sleep 1; call _compiledValue};\
           _marker setMarkerAlpha 1;\
-        };\
-      ";
-      defaultValue = "''";
+        }";
       validate = "expression";
+      defaultValue = "''";
     };
   };
 };
