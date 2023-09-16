801bda02a7d829a726214eafc20986d90df33434
R3voArma3 <robertseibel@outlook.dem>
2023-09-16 15:03:10 +0200

    - Tweaked width of Module Info UI

--- 3denEnhanced/GUI/moduleInformation.hpp
+++ 3denEnhanced/GUI/moduleInformation.hpp
@@ -9,23 +9,23 @@ class ENH_ModuleInformation
     class Header: ctrlStaticTitle
     {
       text = "$STR_ENH_MODULEINFORMATION";
-      x = CENTER_X - 0.5 * WINDOW_PREVIEW_W * GRID_W;
+      x = CENTER_X - 0.5 * WINDOW_W_ATTRIBUTES * GRID_W;
       y = WINDOW_TOP;
-      w = WINDOW_PREVIEW_W * GRID_W;
+      w = WINDOW_W_ATTRIBUTES * GRID_W;
       h = CTRL_DEFAULT_H;
     };
     class Background: ctrlStaticBackground
     {
-      x = CENTER_X - 0.5 * WINDOW_PREVIEW_W * GRID_W;
+      x = CENTER_X - 0.5 * WINDOW_W_ATTRIBUTES * GRID_W;
       y = WINDOW_TOP + CTRL_DEFAULT_H;
-      w = WINDOW_PREVIEW_W * GRID_W;
+      w = WINDOW_W_ATTRIBUTES * GRID_W;
       h = WINDOW_PREVIEW_HAbs;
     };
     class Footer: ctrlStaticFooter
     {
-      x = CENTER_X - 0.5 * WINDOW_PREVIEW_W * GRID_W;
+      x = CENTER_X - 0.5 * WINDOW_W_ATTRIBUTES * GRID_W;
       y = WINDOW_TOP + WINDOW_PREVIEW_HAbs - 2 * GRID_H;
-      w = WINDOW_PREVIEW_W * GRID_W;
+      w = WINDOW_W_ATTRIBUTES * GRID_W;
       h = 7 * GRID_H;
     };
   };
@@ -34,15 +34,15 @@ class ENH_ModuleInformation
     class DescriptionValue: ctrlStructuredText
     {
       idc = IDC_MODULEINFORMATION_DESCRIPTION;
-      x = CENTER_X - 0.5 * WINDOW_PREVIEW_W * GRID_W + 1 * GRID_W;
+      x = CENTER_X - 0.5 * WINDOW_W_ATTRIBUTES * GRID_W + 1 * GRID_W;
       y = WINDOW_TOP + 6 * GRID_H;
-      w = (WINDOW_PREVIEW_W - 2) * GRID_W;
+      w = (WINDOW_W_ATTRIBUTES - 2) * GRID_W;
       h = WINDOW_PREVIEW_HAbs - 9 * GRID_H;
       colorBackground[] = {COLOR_OVERLAY_RGBA};
     };
     class Close: ctrlButtonClose
     {
-      x = CENTER_X - 0.5 * WINDOW_PREVIEW_W * GRID_W + (WINDOW_PREVIEW_W - 26) * GRID_W;
+      x = CENTER_X - 0.5 * WINDOW_W_ATTRIBUTES * GRID_W + (WINDOW_W_ATTRIBUTES - 26) * GRID_W;
       y = WINDOW_TOP + WINDOW_PREVIEW_HAbs - GRID_H;
       w = 25 * GRID_W;
       h = CTRL_DEFAULT_H;
