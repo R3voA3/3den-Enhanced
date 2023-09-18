fe69371226fe8ca061bcf9f252dd8c8d83671190
R3voArma3 <robertseibel@outlook.dem>
2023-09-18 19:02:35 +0200

    - updated UI positioning

--- 3denEnhanced/GUI/moveToLayer.hpp
+++ 3denEnhanced/GUI/moveToLayer.hpp
@@ -10,21 +10,21 @@ class ENH_MoveToLayer
     {
       text = "$STR_ENH_SELECTLAYER_MOVETOLAYER";
       x = CENTER_X - WINDOW_W_ATTRIBUTES * 0.5 * GRID_W;
-      y = 0.5 - WINDOW_HAbs * 0.5 + 5 * GRID_H;
+      y = WINDOW_TOPAbs;
       w = WINDOW_W_ATTRIBUTES * GRID_W;
       h = CTRL_DEFAULT_H;
     };
     class Background: ctrlStaticBackground
     {
       x = CENTER_X - WINDOW_W_ATTRIBUTES * 0.5 * GRID_W;
-      y = 0.5 - WINDOW_HAbs * 0.5 + 10 * GRID_H;
+      y = WINDOW_TOPAbs + CTRL_DEFAULT_H;
       w = WINDOW_W_ATTRIBUTES * GRID_W;
-      h = WINDOW_HAbs - 15 * GRID_H;
+      h = WINDOW_HAbs - 3 * CTRL_DEFAULT_H;
     };
     class Footer: ctrlStaticFooter
     {
       x = CENTER_X - WINDOW_W_ATTRIBUTES * 0.5 * GRID_W;
-      y = 0.5 + WINDOW_HAbs * 0.5 - 12 * GRID_H;
+      y = WINDOW_HAbs - 2 * CTRL_DEFAULT_H - 2 * GRID_H;
       w = WINDOW_W_ATTRIBUTES * GRID_W;
       h = CTRL_DEFAULT_H + 2 * GRID_H;
     };
@@ -36,7 +36,7 @@ class ENH_MoveToLayer
       idc = IDC_MOVETOLAYER_TREE;
       idcSearch = IDC_MOVETOLAYER_SEARCH;
       x = CENTER_X - WINDOW_W_ATTRIBUTES * 0.5 * GRID_W + GRID_W;
-      y = 0.5 - WINDOW_HAbs * 0.5 + 11 * GRID_H;
+      y = WINDOW_TOPAbs + 11 * GRID_H;
       w = WINDOW_W_ATTRIBUTES * GRID_W - 2 * GRID_W;
       h = WINDOW_HAbs - 23 * GRID_H;
       borderSize = 0;
@@ -47,35 +47,35 @@ class ENH_MoveToLayer
     {
       idc = IDC_MOVETOLAYER_SEARCH;
       x = CENTER_X - WINDOW_W_ATTRIBUTES * 0.5 * GRID_W + GRID_W;
-      y = 0.5 + WINDOW_HAbs * 0.5 - 11 * GRID_H;
+      y = WINDOW_HAbs - 2 * CTRL_DEFAULT_H - GRID_H;
       w = 35 * GRID_W;
       h = CTRL_DEFAULT_H;
       onButtonClick = "_this call ENH_fnc_moveToLayer_move";
     };
-  class ButtonSearch: ctrlButtonSearch
+    class ButtonSearch: ctrlButtonSearch
     {
       idc = IDC_MOVETOLAYER_BUTTONSEARCH;
       text = "\a3\3DEN\Data\Displays\Display3DEN\search_start_ca.paa";
       tooltip = "";
       x = CENTER_X - WINDOW_W_ATTRIBUTES * 0.5 * GRID_W + 36 * GRID_W;
-      y = 0.5 + WINDOW_HAbs * 0.5 - 11 * GRID_H;
+      y = WINDOW_HAbs - 2 * CTRL_DEFAULT_H - GRID_H;
       w = 5 * GRID_W;
       h = CTRL_DEFAULT_H;
     };
-    class Move: ctrlButtonOK
+    class ButtonMove: ctrlButtonOK
     {
       idc = -1;
       text = "$STR_A3_BOOT_M02_BIS_SIGN_MARKER";
       x = CENTER_X + WINDOW_W_ATTRIBUTES * 0.5 * GRID_W - 52 * GRID_W;
-      y = 0.5 + WINDOW_HAbs * 0.5 - 11 * GRID_H;
+      y = WINDOW_HAbs - 2 * CTRL_DEFAULT_H - GRID_H;
       w = 25 * GRID_W;
       h = CTRL_DEFAULT_H;
       onButtonClick = "_this call ENH_fnc_moveToLayer_move";
     };
-    class Cancel: ctrlButtonClose
+    class ButtonClose: ctrlButtonClose
     {
       x = CENTER_X + WINDOW_W_ATTRIBUTES * 0.5 * GRID_W - 26 * GRID_W;
-      y = 0.5 + WINDOW_HAbs * 0.5 - 11 * GRID_H;
+      y = WINDOW_HAbs - 2 * CTRL_DEFAULT_H - GRID_H;
       w = 25 * GRID_W;
       h = CTRL_DEFAULT_H;
     };
