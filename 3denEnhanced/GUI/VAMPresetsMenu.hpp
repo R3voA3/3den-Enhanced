53038d44d0c9b35a67779cce3cd922848f54c757
Patrick Orave <oravep@gmail.com>
2023-09-26 15:43:22 -0500

    new preset delete button

--- 3denEnhanced/GUI/VAMPresetsMenu.hpp
+++ 3denEnhanced/GUI/VAMPresetsMenu.hpp
@@ -5,69 +5,78 @@
 class RscButton;
 
 class ENH_VAM_PresetMenu {
-	idd = 12345;
-	
-	class ControlsBackground {
-		class Background: ctrlStaticBackground
-		{
-			x = GUI_GRID_CENTER_X + 0 * GUI_GRID_CENTER_W;
-			y = GUI_GRID_CENTER_Y + 0 * GUI_GRID_CENTER_H;
-			w = 40 * GUI_GRID_CENTER_W;
-			h = 25 * GUI_GRID_CENTER_H;
-		};
-		class Header: ctrlStaticTitle
-		{
-			text = "Virtual Arsenal Manager Presets";
-			x = GUI_GRID_CENTER_X + 0 * GUI_GRID_CENTER_W;
-			y = GUI_GRID_CENTER_Y + 0 * GUI_GRID_CENTER_H;
-			w = 40 * GUI_GRID_CENTER_W;
-			h = 1 * GUI_GRID_CENTER_H;
-		};
-	};
+  idd = 12345;
+  
+  class ControlsBackground {
+    class Background: ctrlStaticBackground
+    {
+      x = GUI_GRID_CENTER_X + 0 * GUI_GRID_CENTER_W;
+      y = GUI_GRID_CENTER_Y + 0 * GUI_GRID_CENTER_H;
+      w = 40 * GUI_GRID_CENTER_W;
+      h = 25 * GUI_GRID_CENTER_H;
+    };
+    class Header: ctrlStaticTitle
+    {
+      text = "Virtual Arsenal Manager Presets";
+      x = GUI_GRID_CENTER_X + 0 * GUI_GRID_CENTER_W;
+      y = GUI_GRID_CENTER_Y + 0 * GUI_GRID_CENTER_H;
+      w = 40 * GUI_GRID_CENTER_W;
+      h = 1 * GUI_GRID_CENTER_H;
+    };
+  };
 
-	class Controls {
-		class Filter: ctrlListNBox
-		{
-			idc = IDC_VAM_PRESET_LISTNBOXFILTER;
-			x = 1 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
-			y = 1 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
-			w = 38 * GUI_GRID_CENTER_W;
-			h = 1 * GUI_GRID_CENTER_H;
-		};
-		class Content: ctrlListNBox
-		{
-			idc = IDC_VAM_PRESET_LISTNBOXCONTENT;
-			x = 1 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
-			y = 2 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
-			w = 38 * GUI_GRID_CENTER_W;
-			h = 20 * GUI_GRID_CENTER_H;
-		};
-		class LoadButton: RscButton
-		{
-			idc = IDC_VAM_PRESET_LOADBUTTON;
-			text = "Load Preset";
-			x = 1 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
-			y = 22.5 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
-			w = 5 * GUI_GRID_CENTER_W;
-			h = 1.5 * GUI_GRID_CENTER_H;
-		};
-		class SaveButton: RscButton
-		{
-			idc = IDC_VAM_PRESET_SAVEBUTTON;
-			text = "Save Preset";
-			x = 7 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
-			y = 22.5 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
-			w = 5 * GUI_GRID_CENTER_W;
-			h = 1.5 * GUI_GRID_CENTER_H;
-		};
-		class Close: ctrlButtonClose
-		{
-			x = 34 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
-			y = 22.5 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
-			w = 5 * GUI_GRID_CENTER_W;
-			h = 1.5 * GUI_GRID_CENTER_H;
-		};
+  class Controls {
+    class Filter: ctrlListNBox
+    {
+      idc = IDC_VAM_PRESET_LISTNBOXFILTER;
+      x = 1 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
+      y = 1 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
+      w = 38 * GUI_GRID_CENTER_W;
+      h = 1 * GUI_GRID_CENTER_H;
+    };
+    class Content: ctrlListNBox
+    {
+      idc = IDC_VAM_PRESET_LISTNBOXCONTENT;
+      x = 1 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
+      y = 2 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
+      w = 38 * GUI_GRID_CENTER_W;
+      h = 20 * GUI_GRID_CENTER_H;
+    };
+    class LoadButton: RscButton
+    {
+      idc = IDC_VAM_PRESET_LOADBUTTON;
+      text = "Load Preset";
+      x = 1 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
+      y = 22.5 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
+      w = 5 * GUI_GRID_CENTER_W;
+      h = 1.5 * GUI_GRID_CENTER_H;
+    };
+    class SaveButton: RscButton
+    {
+      idc = IDC_VAM_PRESET_SAVEBUTTON;
+      text = "Save Preset";
+      x = 7 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
+      y = 22.5 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
+      w = 5 * GUI_GRID_CENTER_W;
+      h = 1.5 * GUI_GRID_CENTER_H;
+    };
+    class DeleteButton: RscButton
+    {
+      idc = IDC_VAM_PRESET_DELETEBUTTON;
+      text = "Delete Preset";
+      x = 13 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
+      y = 22.5 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
+      w = 5 * GUI_GRID_CENTER_W;
+      h = 1.5 * GUI_GRID_CENTER_H;
+    };
+    class Close: ctrlButtonClose
+    {
+      x = 34 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
+      y = 22.5 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
+      w = 5 * GUI_GRID_CENTER_W;
+      h = 1.5 * GUI_GRID_CENTER_H;
+    };
 
-		//...
-	};
+    //...
+  };
 };
