bcd12b2d7d76d1a2e6c7dcc5263a0efb224d1ec1
R3voArma3 <robertseibel@outlook.dem>
2024-05-18 15:29:08 +0200

    - code formatting

--- addons/main/attributesMission/mapIndicators.hpp
+++ addons/main/attributesMission/mapIndicators.hpp
@@ -3,6 +3,6 @@ class ENH_MapIndicators
   displayName = "$STR_ENH_MAIN_ATTRIBUTECATEGORY_MAPINDICATORS";
   property = "ENH_mapIndicators";
   control = "ENH_MapIndicators";
-  expression = "if (!is3DEN && !(_value isEqualTo [false, false, false, false]) && hasInterface) then {disableMapIndicators _value}";
+  expression = "if (!is3DEN && (_value isNotEqualTo [false, false, false, false]) && hasInterface) then {disableMapIndicators _value}";
   defaultValue = "[false, false, false, false]";
 };
