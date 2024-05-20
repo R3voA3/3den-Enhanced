7f690ee1c78596d9039fbaf8f6247c71af3a2aea
R3voArma3 <robertseibel@outlook.dem>
2024-05-20 11:26:13 +0200

    - editor placed objects are now added to zeus as well

--- addons/main/attributesMission/addObjectsToZeus.hpp
+++ addons/main/attributesMission/addObjectsToZeus.hpp
@@ -1,18 +1,20 @@
 class ENH_AddObjectsToZeus
 {
-  displayName = "$STR_ENH_MAIN_EDITABLEOBJZEUS_DISPLAYNAME";
-  tooltip = "$STR_ENH_MAIN_EDITABLEOBJZEUS_TOOLTIP";
-  property = "ENH_AddObjectsToZeus";
-  control = "Checkbox";
-  expression =
-  "\
-    if (!is3DEN && isServer && _value) then\
-    {\
-      addMissionEventHandler ['EntityCreated',\
-      {\
-        params ['_entity'];\
-        allCurators apply {_x addCuratorEditableObjects [[_entity], true]};\
-      }];\
-    }";
-  defaultValue = "false";
+	displayName = "$STR_ENH_MAIN_EDITABLEOBJZEUS_DISPLAYNAME";
+	tooltip = "$STR_ENH_MAIN_EDITABLEOBJZEUS_TOOLTIP";
+	property = "ENH_AddObjectsToZeus";
+	control = "Checkbox";
+	expression =
+	"\
+		if (!is3DEN && isServer && _value) then\
+		{\
+			private _objects = allMissionObjects "";\
+			allCurators apply { _x addCuratorEditableObjects [_objects, true]};\
+			addMissionEventHandler ['EntityCreated',\
+			{\
+				params ['_entity'];\
+				allCurators apply {_x addCuratorEditableObjects [[_entity], true]};\
+			}];\
+		}";
+	defaultValue = "false";
 };
