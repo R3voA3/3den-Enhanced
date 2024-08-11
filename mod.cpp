25c77f2626ffc7259b3017828c363def031d3ad5
R3vo <robertseibel@outlook.de>
2024-08-11 21:00:02 +0200

    - updated mod name

--- mod.cpp
+++ mod.cpp
@@ -6,7 +6,7 @@ hidePicture = 0;
 logo = "x\enh\addons\main\data\logo_ca.paa";
 logoOver = "x\enh\addons\main\data\logoOver_ca.paa";
 logoSmall = "x\enh\addons\main\data\logoSmall_ca.paa";
-name = "3den Enhanced 0.0.0";
+name = "3den Enhanced v0.0.0";
 overview = "Adds new functionalities to the Eden Editor. Mod is not needed for the mission to be played. No dependencies are created.<br/><a href='https://www.bohemia.net/community/licenses/arma-public-license-share-alike'><t underline='true'>This content is under APL-SA license.</t><br/></a><br/><t size='1.5'><img image='x\enh\addons\main\data\APL-SA_Custom_ca.paa' /></t><br/><br/><t size='1.1' color='#11115'>Re-uploading to the Steam Workshop is prohibited!</t>";
 picture = "x\enh\addons\main\data\picture_ca.paa";
 tooltipOwned = "Adds new functionalities to the Eden Editor. Mod is not needed for the mission to be played. No dependencies are created.";
