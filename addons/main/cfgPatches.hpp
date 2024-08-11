f144e2bb92a968cd0f4da46bac4b8033d49dfcd9
R3vo <robertseibel@outlook.de>
2024-08-11 21:00:28 +0200

    - updated addon name

--- addons/main/cfgPatches.hpp
+++ addons/main/cfgPatches.hpp
@@ -4,7 +4,7 @@ class CfgPatches
 	{
 		author = "R3vo";
 		is3DENMod = 1;
-		name = "3den Enhanced";
+		name = "3den Enhanced - Main";
 		requiredAddons[] = {"3den", "A3_Data_F_Decade_Loadorder"};
 		requiredVersion = 2.18;
 		units[] = {};
