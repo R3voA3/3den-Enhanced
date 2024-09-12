b837196683b728158325f0761f6962239e2311ca
R3voA3 <robertseibel@outlook.de>
2024-09-12 10:41:37 +0200

    Improved code

--- addons/main/attributesObject/limitSpeed.hpp
+++ addons/main/attributesObject/limitSpeed.hpp
@@ -4,7 +4,7 @@ class ENH_LimitSpeed
     tooltip = "$STR_ENH_MAIN_LIMITSPEED_TOOLTIP";
     property = "ENH_limitSpeed";
     control = "EditShort";
-    expression = "_this limitSpeed _value";
+    expression = "[_this, _value] remoteExec ['limitSpeed', _this]";
     condition = "objectVehicle";
     typeName = "NUMBER";
     defaultValue = -1;
