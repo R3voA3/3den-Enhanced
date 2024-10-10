4c45c606acacb430fca54cc02921331dac75fe7a
R3voA3 <robertseibel@outlook.de>
2024-10-10 17:15:23 +0200

    tool to clear temp files

---
+++ tools/clearA3Logs.bat
@@ -0,0 +1,5 @@
+del "%localappdata%\Arma 3" /q /s
+del "%localappdata%\Arma 3\AnimDataCache" /q /s
+del "%localappdata%\Arma 3\DataCache" /q /s
+del "%localappdata%\Arma 3\MonetizedServersCache" /q /s
+del "%localappdata%\Arma 3\OfficialServersCache" /q /s
