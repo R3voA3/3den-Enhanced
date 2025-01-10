2be16c3d314f922d109cd4a784b5ea4c9b57d66a
R3voA3 <robertseibel@outlook.de>
2025-01-10 18:40:47 +0100

    minor formatting

--- addons/main/cfgPatches.hpp
+++ addons/main/cfgPatches.hpp
@@ -5,7 +5,7 @@ class CfgPatches
         author = AUTHOR;
         is3DENMod = 1;
         name = "3den Enhanced - Main";
-        requiredAddons[] = {"3den", "A3_Data_F_Decade_Loadorder"};
+        requiredAddons[] = {"3DEN", "A3_Data_F_Decade_Loadorder"};
         requiredVersion = REQUIRED_VERSION;
         url = URL;
         units[] = {};
