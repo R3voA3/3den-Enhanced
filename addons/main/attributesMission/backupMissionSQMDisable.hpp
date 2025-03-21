71e3921d3acc4cb38e32b1a2e39f08fe2860f644
R3voA3 <robertseibel@outlook.de>
2025-03-21 09:47:33 +0100

    added translation to new attribute

--- addons/main/attributesMission/backupMissionSQMDisable.hpp
+++ addons/main/attributesMission/backupMissionSQMDisable.hpp
@@ -1,7 +1,7 @@
 class ENH_BackupMissionSQMDisable
 {
-    displayName = "Disable Mission SQM Backup";
-    tooltip = "Disables mission SQM backup for this scenario.";
+    displayName = "$STR_ENH_MAIN_BACKUPMISSIONSQM_DISABLE_DISPLAYNAME";
+    tooltip = "$STR_ENH_MAIN_BACKUPMISSIONSQM_DISABLE_TOOLTIP";
     property = "ENH_BackupMissionSQMDisable";
     control = "Checkbox";
     expression = "";
