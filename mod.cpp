bed443488c01cc9f61a4cf1c5611f9c671287c27
R3voA3 <robertseibel@outlook.de>
2025-03-23 16:53:01 +0100

    removed license statement from mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -7,6 +7,6 @@ logo = "x\enh\addons\main\data\logo_ca.paa";
 logoOver = "x\enh\addons\main\data\logoOver_ca.paa";
 logoSmall = "x\enh\addons\main\data\logoSmall_ca.paa";
 name = "3den Enhanced v0.0.0";
-overview = "This modification adds new functionalities to the Eden Editor. The modification is not needed for the scenarios to be played as no dependencies are created.<br/><a href='https://www.bohemia.net/community/licenses/arma-public-license-share-alike'><t underline='true'>This content is under APL-SA license.</t><br/></a><br/><t size='1.5'><img image='x\enh\addons\main\data\APL-SA_Custom_ca.paa' /></t>";
+overview = "This modification adds new functionalities to the Eden Editor. The modification is not needed for the scenarios to be played as no dependencies are created.";
 picture = "x\enh\addons\main\data\picture_ca.paa";
 tooltipOwned = "This modification adds new functionalities to the Eden Editor. The modification is not needed for the scenarios to be played as no dependencies are created.";
