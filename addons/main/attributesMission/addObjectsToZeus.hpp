7784412b60ff0132cc3114262024fa03dfe442f7
R3voA3 <robertseibel@outlook.de>
2025-03-29 13:37:12 +0100

    added prototype for hemtt attribute code checking

--- addons/main/attributesMission/addObjectsToZeus.hpp
+++ addons/main/attributesMission/addObjectsToZeus.hpp
@@ -5,29 +5,30 @@ class ENH_AddObjectsToZeus
     tooltip = "$STR_ENH_MAIN_EDITABLEOBJZEUS_TOOLTIP";
     property = "ENH_AddObjectsToZeus";
     control = "Combo";
-    expression =
-    "\
-        if (_value isEqualType true) then {_value = 2};\
-        if (!is3DEN && isServer && _value > 0) then\
-        {\
-            _value spawn
-            {
-                params ['_value'];
-                waitUntil {sleep 1; allCurators isNotEqualTo []};
-                if (_value == 1 || _value == 2) then\
-                {\
-                    allCurators apply {_x addCuratorEditableObjects [allMissionObjects '', true]};\
-                };\
-                if (_value == 2) then\
-                {\
-                    addMissionEventHandler ['EntityCreated',\
-                    {\
-                        params ['_entity'];\
-                        allCurators apply {_x addCuratorEditableObjects [[_entity], true]};\
-                    }];\
-                };\
-            };\
-        }";
+    expression = "EXPRESSION_PLACEHOLDER";
+    // expression =
+    // "\
+    //     if (_value isEqualType true) then {_value = 2};\
+    //     if (!is3DEN && isServer && _value > 0) then\
+    //     {\
+    //         _value spawn
+    //         {
+    //             params ['_value'];
+    //             waitUntil {sleep 1; allCurators isNotEqualTo []};
+    //             if (_value == 1 || _value == 2) then\
+    //             {\
+    //                 allCurators apply {_x addCuratorEditableObjects [allMissionObjects '', true]};\
+    //             };\
+    //             if (_value == 2) then\
+    //             {\
+    //                 addMissionEventHandler ['EntityCreated',\
+    //                 {\
+    //                     params ['_entity'];\
+    //                     allCurators apply {_x addCuratorEditableObjects [[_entity], true]};\
+    //                 }];\
+    //             };\
+    //         };\
+    //     }";
     defaultValue = 0;
     typeName = "NUMBER";
 
