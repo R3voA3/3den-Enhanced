1b99b78e540fc13132be964c186853b84f5717e8
R3voA3 <robertseibel@outlook.de>
2025-06-07 16:50:50 +0200

    added todo tag

--- addons/main/attributesEditor/holdActionIcons.hpp
+++ addons/main/attributesEditor/holdActionIcons.hpp
@@ -1,7 +1,7 @@
 class ENH_HoldActionIcons
 {
-    displayName = "Hold Action Icons";
-    tooltip = "Define additional icons here that can be used for the hold action attribute.";
+    displayName = "Hold Action Icons"; // TODO: localize 2025-06-07 R3vo
+    tooltip = "Define additional icons here that can be used for the hold action attribute."; // TODO: localize 2025-06-07 R3vo
     property = "ENH_HoldActionIcons";
     control = "ENH_InputList";
     expression = "profileNamespace setVariable ['ENH_EditorPreferences_HoldActionIcons', _value]";
