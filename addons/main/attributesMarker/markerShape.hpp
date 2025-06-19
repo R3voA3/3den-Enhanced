1cc63da4fa649c9603e123d1a2c0eb10b3ef5bef
R3voA3 <robertseibel@outlook.de>
2025-06-19 15:12:39 +0200

    fixed issues with translations and added new ones

--- addons/main/attributesMarker/markerShape.hpp
+++ addons/main/attributesMarker/markerShape.hpp
@@ -1,7 +1,7 @@
 class ENH_MarkerShape
 {
-    displayName = "Custom Shape";
-    tooltip = "";
+    displayName = "$STR_ENH_MAIN_MARKER_SHAPE_DISPLAYNAME";
+    tooltip = "$STR_ENH_MAIN_ATTRIBUTES_MARKER_SHAPE_DISABLED";
     property = "ENH_markerShape";
     control = "ENH_MarkerShape";
     expression = "if (_value != 'NONE' && isServer) then {_this setMarkerShape _value}";
