ae9da1c16513be0e6e1ff5005a5734f7015385c0
R3vo <robertseibel@outlook.de>
2026-01-23 19:54:38 +0100

    commented test module

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -44,136 +44,136 @@ class ENH_DisplayEmpty
     class Controls {};
 };
 
-#ifdef ISDEV
-class CfgVehicles
-{
-    class Module_F;
-    class ModuleMPType_F: Module_F
-    {
-        class ModuleDescription;
-    };
-    class ENH_TestModule: ModuleMPType_F
-    {
-        author = "Bohemia Interactive";
-        _generalMacro = "ModuleMPTypeSeize_F";
-        scope = 2;
-        icon = "\a3\Missions_F_Gamma\data\img\iconMPTypeSeize_ca.paa";
-        portrait = "\a3\Missions_F_Gamma\data\img\portraitMPTypeSeize_ca.paa";
-        function = "BIS_fnc_ModuleMPTypeSeize";
-        functionPriority = 1;
-        class Arguments
-        {
-            class RatioInfantry
-            {
-                displayName = "Infantry priority";
-                description = "How much the system will prioritize spawning infantry";
-                defaultValue = "1";
-            };
-            class RatioGround
-            {
-                displayName = "Ground vehicles priority";
-                description = "How much the system will prioritize spawning ground vehicles";
-                defaultValue = "1";
-            };
-            class RatioAir
-            {
-                displayName = "Aircrafts priority";
-                description = "How much the system will prioritize spawning aircraft";
-                defaultValue = "1";
-            };
-            class RatioWater
-            {
-                displayName = "Naval priority";
-                description = "How much the system will prioritize spawning sea crafts";
-                defaultValue = "1";
-            };
-            class CheatCoef
-            {
-                displayName = "Auto coef";
-                description = "Cost coefficient applied when no Zeus is present and the system creates enemies automatically.";
-                defaultValue = "1";
-            };
-        };
-        class ModuleDescription: ModuleDescription
-        {
-            description[] = {"Create Seize mission mode, compatible both with single- and multiplayer.","","Players are capturing AI controlled sectors.","When they seize all of them, they win.","If they run out of time or respawn tickets, they lose","","Note: Curator spawning costs and limited time or respawns have to be configured separately."};
-            sync[] = {"ModuleSector_F","SidePlayers","SideEnemy"};
-            class SidePlayers
-            {
-                description = "Side of attacking players.";
-                displayName = "Player Side";
-                vehicle = "SideBLUFOR_F";
-                duplicate = 1;
-                sync[] = {"ModuleSector_F"};
-            };
-            class s1
-            {
-                description = "Side of defending forces, recognized by presence of synced curator.";
-                displayName = "Enemy Side";
-                vehicle = "SideOPFOR_F";
-                // sync[] = {"Curator_F"};
-            };
-            class s2
-            {
-                description = "Side of defending forces, recognized by presence of synced curator.";
-                displayName = "Enemy Side";
-                vehicle = "SideOPFOR_F";
-                // sync[] = {"Curator_F"};
-            };
-            class s3
-            {
-                description = "Side of defending forces, recognized by presence of synced curator.";
-                displayName = "Enemy Side";
-                vehicle = "SideOPFOR_F";
-            };
-            class s4
-            {
-                description = "Side of defending forces, recognized by presence of synced curator.";
-                displayName = "Enemy Side";
-                vehicle = "SideOPFOR_F";
-            };
-            class Curator_F
-            {
-                description = "Mark the side as enemy, curator controlled. At least one side has to be marked this way. All other sides will remain friendly, player controlled.";
-                optional = 1;
-                sync[] = {"ModuleSector_F"};
-            };
-            class ModuleSector_F
-            {
-                description = "Add a contested sector.";
-                duplicate = 1;
-                sync[] = {"LocationArea_F","MiscLock_F", "s1", "s2", "s3", "s4"};
-            };
-            class LocationArea_F
-            {
-                description = "";
-                duplicate = 1;
-                sync[] = {"TriggerArea"};
-            };
-            class TriggerArea
-            {
-                position = 1;
-                direction = 1;
-                description = "Set the sector area. Enemy units will be spawned only in this area. When the sector is captured by a friendly side, the trigger will be activated, potentially triggering other modules synced to it (e.g., respawn module).";
-                duplicate = 1;
-                vehicle = "EmptyDetector";
-            };
-            class MiscLock_F
-            {
-                description = "";
-                optional = 1;
-                sync[] = {"TriggerLock"};
-            };
-            class TriggerLock
-            {
-                position = 1;
-                direction = 1;
-                description = "Disable enemy unit spawning in the sector when friendly units are within this area. Re-enable it in case they are gone again.";
-                duplicate = 1;
-                vehicle = "EmptyDetector";
-            };
-        };
-        displayName = "TEST";
-    };
-};
-#endif
+// #ifdef ISDEV
+// class CfgVehicles
+// {
+//     class Module_F;
+//     class ModuleMPType_F: Module_F
+//     {
+//         class ModuleDescription;
+//     };
+//     class ENH_TestModule: ModuleMPType_F
+//     {
+//         author = "Bohemia Interactive";
+//         _generalMacro = "ModuleMPTypeSeize_F";
+//         scope = 2;
+//         icon = "\a3\Missions_F_Gamma\data\img\iconMPTypeSeize_ca.paa";
+//         portrait = "\a3\Missions_F_Gamma\data\img\portraitMPTypeSeize_ca.paa";
+//         function = "BIS_fnc_ModuleMPTypeSeize";
+//         functionPriority = 1;
+//         class Arguments
+//         {
+//             class RatioInfantry
+//             {
+//                 displayName = "Infantry priority";
+//                 description = "How much the system will prioritize spawning infantry";
+//                 defaultValue = "1";
+//             };
+//             class RatioGround
+//             {
+//                 displayName = "Ground vehicles priority";
+//                 description = "How much the system will prioritize spawning ground vehicles";
+//                 defaultValue = "1";
+//             };
+//             class RatioAir
+//             {
+//                 displayName = "Aircrafts priority";
+//                 description = "How much the system will prioritize spawning aircraft";
+//                 defaultValue = "1";
+//             };
+//             class RatioWater
+//             {
+//                 displayName = "Naval priority";
+//                 description = "How much the system will prioritize spawning sea crafts";
+//                 defaultValue = "1";
+//             };
+//             class CheatCoef
+//             {
+//                 displayName = "Auto coef";
+//                 description = "Cost coefficient applied when no Zeus is present and the system creates enemies automatically.";
+//                 defaultValue = "1";
+//             };
+//         };
+//         class ModuleDescription: ModuleDescription
+//         {
+//             description[] = {"Create Seize mission mode, compatible both with single- and multiplayer.","","Players are capturing AI controlled sectors.","When they seize all of them, they win.","If they run out of time or respawn tickets, they lose","","Note: Curator spawning costs and limited time or respawns have to be configured separately."};
+//             sync[] = {"ModuleSector_F","SidePlayers","SideEnemy"};
+//             class SidePlayers
+//             {
+//                 description = "Side of attacking players.";
+//                 displayName = "Player Side";
+//                 vehicle = "SideBLUFOR_F";
+//                 duplicate = 1;
+//                 sync[] = {"ModuleSector_F"};
+//             };
+//             class s1
+//             {
+//                 description = "Side of defending forces, recognized by presence of synced curator.";
+//                 displayName = "Enemy Side";
+//                 vehicle = "SideOPFOR_F";
+//                 // sync[] = {"Curator_F"};
+//             };
+//             class s2
+//             {
+//                 description = "Side of defending forces, recognized by presence of synced curator.";
+//                 displayName = "Enemy Side";
+//                 vehicle = "SideOPFOR_F";
+//                 // sync[] = {"Curator_F"};
+//             };
+//             class s3
+//             {
+//                 description = "Side of defending forces, recognized by presence of synced curator.";
+//                 displayName = "Enemy Side";
+//                 vehicle = "SideOPFOR_F";
+//             };
+//             class s4
+//             {
+//                 description = "Side of defending forces, recognized by presence of synced curator.";
+//                 displayName = "Enemy Side";
+//                 vehicle = "SideOPFOR_F";
+//             };
+//             class Curator_F
+//             {
+//                 description = "Mark the side as enemy, curator controlled. At least one side has to be marked this way. All other sides will remain friendly, player controlled.";
+//                 optional = 1;
+//                 sync[] = {"ModuleSector_F"};
+//             };
+//             class ModuleSector_F
+//             {
+//                 description = "Add a contested sector.";
+//                 duplicate = 1;
+//                 sync[] = {"LocationArea_F","MiscLock_F", "s1", "s2", "s3", "s4"};
+//             };
+//             class LocationArea_F
+//             {
+//                 description = "";
+//                 duplicate = 1;
+//                 sync[] = {"TriggerArea"};
+//             };
+//             class TriggerArea
+//             {
+//                 position = 1;
+//                 direction = 1;
+//                 description = "Set the sector area. Enemy units will be spawned only in this area. When the sector is captured by a friendly side, the trigger will be activated, potentially triggering other modules synced to it (e.g., respawn module).";
+//                 duplicate = 1;
+//                 vehicle = "EmptyDetector";
+//             };
+//             class MiscLock_F
+//             {
+//                 description = "";
+//                 optional = 1;
+//                 sync[] = {"TriggerLock"};
+//             };
+//             class TriggerLock
+//             {
+//                 position = 1;
+//                 direction = 1;
+//                 description = "Disable enemy unit spawning in the sector when friendly units are within this area. Re-enable it in case they are gone again.";
+//                 duplicate = 1;
+//                 vehicle = "EmptyDetector";
+//             };
+//         };
+//         displayName = "TEST";
+//     };
+// };
+// #endif
