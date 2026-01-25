5945827e32343c48fcde7719b31c1bc78ba417e1
R3vo <robertseibel@outlook.de>
2026-01-25 16:11:48 +0100

    added death and arsenal loadout restore options

--- addons/main/cfg3DEN/attributesMission/saveLoadout.hpp
+++ addons/main/cfg3DEN/attributesMission/saveLoadout.hpp
@@ -3,19 +3,74 @@ class ENH_SaveLoadout
     displayName = "$STR_ENH_MAIN_SAVELOADOUT_DISPLAYNAME";
     tooltip = "$STR_ENH_MAIN_SAVELOADOUT_TOOLTIP";
     property = "ENH_saveLoadout";
-    control = "Checkbox";
+    control = "Combo";
     expression =
-    "if (!isMultiplayer || !_value || is3DEN || !hasInterface) exitWith {};\
-        spawn\
+    "if (!isMultiplayer || _value == 0 || is3DEN || !hasInterface) exitWith {};\
+    if (_value isEqualType true) then {_value = [0, 1] select _value};\
+    _value spawn\
+    {\
+        params ['_value'];\
+        waitUntil {sleep 1; !isNull player};\
+        switch _value do\
         {\
-            waitUntil {sleep 1; !isNull player};\
-            player setVariable ['ENH_savedLoadout', getUnitloadout player];\
-            player addMPEventHandler ['MPRespawn',\
+            case 1:\
             {\
-                params ['_unit'];\
-                private _loadout = _unit getVariable ['ENH_savedLoadout', []];\
-                if (_loadout isNotEqualTo []) then {_unit setUnitLoadout _loadout};\
-            }];\
-        };";
-    defaultValue = "false";
+                player setVariable ['ENH_SavedLoadout', getUnitloadout player];\
+                player addEventHandler ['Respawn',\
+                {\
+                    private _loadout = player getVariable ['ENH_SavedLoadout', []];\
+                    if (_loadout isNotEqualTo []) then {player setUnitLoadout _loadout};\
+                }];\
+            };\
+            case 2:\
+            {\
+                player addEventHandler ['Killed',  {player setVariable ['ENH_SavedLoadout', getUnitLoadout player]}];\
+                player addEventHandler ['Respawn',\
+                {\
+                    private _loadout = player getVariable ['ENH_SavedLoadout', []];\
+                    if (_loadout isNotEqualTo []) then {player setUnitLoadout _loadout};\
+                }];\
+            };\
+            case 3:\
+            {
+                player setVariable ['ENH_SavedLoadout', getUnitloadout player];\
+                player addEventHandler ['Respawn',\
+                {\
+                    private _loadout = player getVariable ['ENH_SavedLoadout', []];\
+                    if (_loadout isNotEqualTo []) then {player setUnitLoadout _loadout};\
+                }];\
+                \
+                [missionNamespace, 'arsenalClosed', {player setVariable ['ENH_SavedLoadout', getUnitloadout player]}] call BIS_fnc_addScriptedEventHandler;\
+            };
+            default {};\
+        };\
+    };";
+    defaultValue = 0;
+    typeName = "NUMBER";
+    class Values
+    {
+        class Disabled
+        {
+            name = "$STR_A3_DISABLED";
+            value = 0;
+        };
+        class OriginalLoadout
+        {
+            name = "$STR_ENH_MAIN_SAVELOADOUT_ORIGINAL_LOADOUT_DISPLAYNAME";
+            tooltip = "$STR_ENH_MAIN_SAVELOADOUT_ORIGINAL_LOADOUT_TOOLTIP";
+            value = 1;
+        };
+        class DeathLoadout
+        {
+            name = "$STR_ENH_MAIN_SAVELOADOUT_DEATH_LOADOUT_DISPLAYNAME";
+            tooltip = "$STR_ENH_MAIN_SAVELOADOUT_DEATH_LOADOUT_TOOLTIP";
+            value = 2;
+        };
+        class ArsenalLoadout
+        {
+            name = "$STR_ENH_MAIN_SAVELOADOUT_ARSENAL_LOADOUT_DISPLAYNAME";
+            tooltip = "$STR_ENH_MAIN_SAVELOADOUT_ARSENAL_LOADOUT_TOOLTIP";
+            value = 3;
+        };
+    };
 };
