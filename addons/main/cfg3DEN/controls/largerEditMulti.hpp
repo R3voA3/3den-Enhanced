50cd238b98f2cbd83343150a221536a59c2d5b67
R3vo <robertseibel@outlook.de>
2026-08-22 14:08:30 +0200

    increased comment description box size (#528)
    * increased comment description box size
    
    * simplefied config for larger multi controls

--- addons/main/cfg3DEN/controls/largerEditMulti.hpp
+++ addons/main/cfg3DEN/controls/largerEditMulti.hpp
@@ -6,31 +6,32 @@ class Edit: Title
         class Value;
     };
 };
-class EditMulti3: Edit
+class EditMulti3: Edit // We leave this one unchanged as it's not used anyway
 {
     class Controls: Controls
     {
-        class Value: Value
-        {
-            h = QUOTE(69 * GRID_H);
-        };
-        class Background: ctrlStaticOverlay
-        {
-            h = QUOTE(73 * GRID_H);
-        };
-        class Title: ctrlStaticFrame
-        {
-            h = QUOTE(75 * GRID_H);
-        };
+        class Background;
+        class Title;
+        class Value;
     };
 };
 class EditMulti5: EditMulti3
 {
+    h = QUOTE(75 * GRID_H);
     class Controls: Controls
     {
-        class Value;
-        class Background;
-        class Title;
+        class Background: Background
+        {
+            h = QUOTE(73 * GRID_H);
+        };
+        class Title: Title
+        {
+            h = QUOTE(75 * GRID_H);
+        };
+        class Value: Value
+        {
+            h = QUOTE(69 * GRID_H);
+        };
     };
 };
 class EditCodeMulti3: EditMulti3
@@ -42,13 +43,13 @@ class EditCodeMulti3: EditMulti3
         {
             h = QUOTE(55.5 * GRID_H);
         };
-        class Value: Value
-        {
-            h = QUOTE(52.5 * GRID_H - GRID_H);
-        };
         class Title: Title
         {
             h = QUOTE(57.5 * GRID_H);
+        };
+        class Value: Value
+        {
+            h = QUOTE(52.5 * GRID_H - GRID_H);
         };
     };
 };
@@ -61,13 +62,13 @@ class EditCodeMulti5: EditMulti5
         {
             h = QUOTE(73 * GRID_H);
         };
-        class Value: Value
-        {
-            h = QUOTE(69 * GRID_H);
-        };
         class Title: Title
         {
             h = QUOTE(75 * GRID_H);
+        };
+        class Value: Value
+        {
+            h = QUOTE(69 * GRID_H);
         };
     };
 };
